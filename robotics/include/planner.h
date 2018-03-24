8ec1a209be9234ede9eb4c47e1b00c1197ffdbc2
Gilles DOFFE <g.doffe@gmail.com>
2018-03-24 01:04:42 +0100

    robotics: Move path_t and path_pose_t to path.h
    Move path_t and path_pose_t from planner.h to path.h
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- robotics/include/planner.h
+++ robotics/include/planner.h
@@ -2,23 +2,9 @@
 #define PLANNER_H_
 
 #include "odometry.h"
+#include "path.h"
 #include "utils.h"
 
-typedef struct {
-	pose_t pos;
-	uint8_t allow_reverse;
-	func_cb_t act;
-} path_pose_t;
-
-typedef struct {
-	/* static cfg */
-	uint8_t play_in_loop; /* for unit tests */
-	uint8_t nb_pose;
-	path_pose_t *poses;
-
-	/* dynamic variables */
-	uint8_t current_pose_idx;
-} path_t;
 
 pose_t planner_get_path_pose_initial(void);
 void planner_start_game(void);
