83fdd1163f20a8fe61600dc28a949d18f83af8e8
Gilles DOFFE <g.doffe@gmail.com>
2018-04-05 11:27:52 +0200

    calibration: debug calibration
    * correct controller_set_mode() call to avoid segmentation fault
    * initialize controller into CTRL_STATE_STOP mode
    * do not start RIOT shell
    * launch calibration task in main thread
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- robotics/platform_task.c
+++ robotics/platform_task.c
@@ -20,18 +20,18 @@ char calib_thread_stack[THREAD_STACKSIZE_DEFAULT];
 void mach_tasks_init(void)
 {
 /* FIXME: Launch calibration task */
-#if defined(MODULE_CALIBRATION)
-    thread_create(calib_thread_stack, sizeof(calib_thread_stack),
-                  0, 0,
-                  task_calibration_entry, NULL, "calibration");
-#else
-	planner_start_game();
-#endif /* MODULE_CALIBRATION */
     thread_create(controller_thread_stack, sizeof(controller_thread_stack),
                   0, 0,
                   task_controller_update, NULL, "motion_ctrl");
-
     thread_create(planner_thread_stack, sizeof(planner_thread_stack),
                   0, 0,
                   task_planner, NULL, "game_planner");
+#if defined(MODULE_CALIBRATION)
+    /*thread_create(calib_thread_stack, sizeof(calib_thread_stack),
+                  0, 0,
+                  task_calibration_entry, NULL, "calibration");*/
+    task_calibration_entry(NULL);
+#else
+	planner_start_game();
+#endif /* MODULE_CALIBRATION */
 }
