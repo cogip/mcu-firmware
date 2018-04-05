11c2bc59715a241bdaa06f1ab2f8f0bd6f96ef60
Gilles DOFFE <g.doffe@gmail.com>
2018-04-05 13:04:58 +0200

    robotics: update default path
    Update default path to something simpler.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- robotics/path.c
+++ robotics/path.c
@@ -6,16 +6,18 @@
 
 // TODO: do not use Rear gear.
 static path_pose_t poses[] = {
-	{ .pos = {.x =  873, .y =  163, .O =   45, }, .act = NULL, }, /* POSE_INITIAL */
-	{ .pos = {.x = 1400, .y =  500, .O =  135, }, .act = NULL, },
-	{ .pos = {.x = 1000, .y =  900, .O =  -90, }, .act = NULL, },
-	{ .pos = {.x =  950, .y =  400, .O =  -90, }, .act = NULL, },
+	{ .pos = {.x =    0, .y =  0, .O =   0, }, .allow_reverse = TRUE, .act = NULL, }, /* POSE_INITIAL */
+	{ .pos = {.x = 1000, .y =  0, .O =  90, }, .allow_reverse = TRUE, .act = NULL, },
+	{ .pos = {.x = 1000, .y =  500, .O =  90, }, .allow_reverse = TRUE, .act = NULL, },
+	{ .pos = {.x = 1000, .y =  -500, .O =  90, }, .allow_reverse = TRUE, .act = NULL, },
+	{ .pos = {.x = 1000, .y =  0, .O =  0, }, .allow_reverse = TRUE, .act = NULL, },
+	{ .pos = {.x =    0, .y =  0, .O =   0, }, .allow_reverse = TRUE, .act = NULL, }, /* POSE_INITIAL */
 };
 
 path_t robot_path = {
 	/* static cfg */
 	.play_in_loop = FALSE,
-	.nb_pose = 4,
+	.nb_pose = 6,
 	.poses = poses,
 };
 
