b60937de8ae3855eaf124262f7cc27b35349b3dd
Gilles DOFFE <g.doffe@gmail.com>
2018-04-09 16:05:31 +0200

    robotics: add countdown to enter calibration menu
    * add 3 seconds countdown (CALIBRATION_BOOT_DELAY macro)
    * game is started automatically at countdown end
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- robotics/platform_task.c
+++ robotics/platform_task.c
@@ -4,6 +4,7 @@
 #include "planner.h"
 #include "platform.h"
 #include "platform_task.h"
+#include "xtimer.h"
 #include <thread.h>
 
 //FIXME:
@@ -11,12 +12,67 @@
 
 char controller_thread_stack[THREAD_STACKSIZE_DEFAULT];
 char planner_thread_stack[THREAD_STACKSIZE_DEFAULT];
+
 #if defined(MODULE_CALIBRATION)
+#define CALIBRATION_BOOT_DELAY 3
+
 char calib_thread_stack[THREAD_STACKSIZE_DEFAULT];
+char calib_wait_thread_stack[THREAD_STACKSIZE_DEFAULT];
+
+typedef enum
+{
+	CALIBRATION_NONE = 0,
+	CALIBRATION_READY,
+	CALIBRATION_STARTED,
+} calibration_state_t;
+
+/* Calibration flag to launch or not calibration thread */
+static volatile calibration_state_t calib_flag = 0;
+
+/**
+ * @brief Wait for calibration to be started
+ *
+ * @param[in] arg		thread args (not used)
+ *
+ * @return				0 for success
+ */
+static void* calib_wait(void* arg)
+{
+	(void)arg;
+
+	/* Thread period */
+	xtimer_ticks32_t loop_start_time = xtimer_now();
+	/* start time and current time to check if CALIBRATION_BOOT_DELAY is exceeded */
+	uint32_t start_time = xtimer_now_usec();
+	uint32_t current_time = xtimer_now_usec();
+	/* Counter for display purpose */
+	uint8_t sec_count = 0;
+
+	/* Wait until calibration flag is raised in calling thread or delay is exceeded */
+	while ((calib_flag == CALIBRATION_NONE)
+		&& (current_time - start_time < CALIBRATION_BOOT_DELAY * US_PER_SEC))
+	{
+		current_time = xtimer_now_usec();
+		xtimer_periodic_wakeup(&loop_start_time, THREAD_PERIOD_INTERVAL);
+		if (current_time > sec_count * US_PER_SEC)
+			printf("%u seconds left...\n", CALIBRATION_BOOT_DELAY - sec_count++);
+	}
+
+	/* On calibration flag raised, calibration thread is ready */
+	if (calib_flag == CALIBRATION_READY)
+	{
+		puts("Calibration started !");
+		/* Up calibration flag to start calibration task */
+		calib_flag = CALIBRATION_STARTED;
+	}
+	/* else launch game */
+	else
+		planner_start_game();
+
+	return 0;
+}
 #endif /* MODULE_CALIBRATION */
 
-/* Note: last task registered will be the first to be scheduled.
- *	running order can be important in current tasks design */
 void mach_tasks_init(void)
 {
 /* FIXME: Launch calibration task */
@@ -27,9 +83,17 @@ void mach_tasks_init(void)
 				  0, 0,
 				  task_planner, NULL, "game_planner");
 #if defined(MODULE_CALIBRATION)
-	/*thread_create(calib_thread_stack, sizeof(calib_thread_stack),
-				  0, 0,
-				  task_calibration_entry, NULL, "calibration");*/
+	thread_create(calib_wait_thread_stack, sizeof(calib_wait_thread_stack),
+		0, 0,
+		calib_wait, NULL, "wait_calibration");
+
+	puts("Press a key to enter calibration...");
+	getchar();
+	/* Calibration is ready */
+	calib_flag = CALIBRATION_READY;
+	/* Wait for calibration to be started */
+	while (calib_flag < CALIBRATION_STARTED);
+	/* Launch calibration function */
 	task_calibration_entry(NULL);
 #else
 	planner_start_game();
