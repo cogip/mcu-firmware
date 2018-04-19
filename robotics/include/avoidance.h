7dfd5036a07988709cd4d70f0c7ed7b8efc553ba
Gilles DOFFE <g.doffe@gmail.com>
2018-04-19 11:36:50 +0200

    robotics/avoidance: support dynamic obstacles
    * handle one dynamic obstacle
    * manage border case when robot is inside an obstacle
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- robotics/include/avoidance.h
+++ robotics/include/avoidance.h
@@ -29,11 +29,14 @@ typedef struct
 
 pose_t dijkstra(uint16_t target, uint16_t index);
 pose_t avoidance(uint8_t index);
+double distance_points(pose_t *a, pose_t *b);
 int update_graph(void/*const pose_t *start, const pose_t *finish*/);
 void set_start_finish(const pose_t *s, const pose_t *f);
 void init_polygons(void);
 void build_avoidance_graph(void);
 int add_polygon(polygon_t *polygon);
+int add_dyn_polygon(polygon_t *polygon);
+void reset_dyn_polygons(void);
 uint8_t is_point_in_polygon(const polygon_t *polygons,pose_t p);
 int8_t get_point_index_in_polygon(const polygon_t *polygons,pose_t p);
 uint8_t is_segment_crossing_line(pose_t a, pose_t b, pose_t o, pose_t p);
--- robotics/obstacle.c
+++ robotics/obstacle.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include "avoidance.h"
+#include "platform.h"
+#include "trigonometry.h"
+#include "obstacle.h"
+#include <math.h>
 
 /* Init all known fixed obstacles on map */
 void mach_fixed_obstacles_init(void)
@@ -19,3 +23,55 @@ void mach_fixed_obstacles_init(void)
 	}
 }
 
+/* Init all known fixed obstacles on map */
+void add_dyn_obstacle(const pose_t *robot_pose/*, analog_sensor_zone_t zone*/)
+{
+	static polygon_t polygon;
+	uint8_t nb_vertices;
+	//double direction = -1;
+
+	pose_t robot_pose_tmp = *robot_pose;
+	robot_pose_tmp.O /= PULSE_PER_DEGREE;
+	robot_pose_tmp.O = DEG2RAD(robot_pose_tmp.O);
+	robot_pose_tmp.x /= PULSE_PER_MM;
+	robot_pose_tmp.y /= PULSE_PER_MM;
+
+	polygon.count = 0;
+	nb_vertices = 4;
+	if (nb_vertices < POLY_MAX_POINTS)
+	{
+
+		/*if (zone == AS_ZONE_REAR)
+		{
+			direction = 1;
+		}*/
+
+		/* Right reference*/
+		pose_t ref_pos_right = (pose_t){.x = robot_pose_tmp.x + OBSTACLE_DYN_SIZE * cos(robot_pose_tmp.O - M_PI/2),
+							 .y = robot_pose_tmp.y + OBSTACLE_DYN_SIZE * sin(robot_pose_tmp.O - M_PI/2),
+							 .O = robot_pose_tmp.O /* + direction * M_PI/2 */};
+		/* Left reference */
+		pose_t ref_pos_left = (pose_t){.x = robot_pose_tmp.x + OBSTACLE_DYN_SIZE * cos(robot_pose_tmp.O + M_PI/2),
+							 .y = robot_pose_tmp.y + OBSTACLE_DYN_SIZE * sin(robot_pose_tmp.O + M_PI/2),
+							 .O = robot_pose_tmp.O /* + direction * M_PI/2 */};
+
+		/* Right points */
+		polygon.points[polygon.count] = (pose_t){.x = ref_pos_right.x + OBSTACLE_DYN_LENGTH * cos(ref_pos_right.O),
+							   .y = ref_pos_right.y + OBSTACLE_DYN_LENGTH * sin(ref_pos_right.O)};
+		polygon.count++;
+		polygon.points[polygon.count] = (pose_t){.x = ref_pos_right.x + (OBSTACLE_DYN_LENGTH + OBSTACLE_DYN_SIZE) * cos(ref_pos_right.O),
+							 .y = ref_pos_right.y + (OBSTACLE_DYN_LENGTH + OBSTACLE_DYN_SIZE) * sin(ref_pos_right.O)};
+		polygon.count++;
+
+		/* Left points */
+		polygon.points[polygon.count] = (pose_t){.x = ref_pos_left.x + (OBSTACLE_DYN_LENGTH + OBSTACLE_DYN_SIZE) * cos(ref_pos_left.O),
+							 .y = ref_pos_left.y + (OBSTACLE_DYN_LENGTH + OBSTACLE_DYN_SIZE) * sin(ref_pos_left.O)};
+		polygon.count++;
+		polygon.points[polygon.count] = (pose_t){.x = ref_pos_left.x + OBSTACLE_DYN_LENGTH * cos(ref_pos_left.O),
+							 .y = ref_pos_left.y + OBSTACLE_DYN_LENGTH * sin(ref_pos_left.O)};
+		polygon.count++;
+		cons_printf("@o@,%+.0f,%+.0f,%+.0f,%+.0f,%+.0f,%+.0f,%+.0f,%+.0f,%+.0f\n", polygon.points[0].x, polygon.points[0].y, polygon.points[1].x, polygon.points[1].y, polygon.points[2].x, polygon.points[2].y, polygon.points[3].x, polygon.points[3].y, robot_pose->O/PULSE_PER_DEGREE);
+		cons_printf("@t@,%+.0f,%+.0f,%+.0f,%+.0f\n", ref_pos_right.x, ref_pos_right.y, ref_pos_left.x, ref_pos_left.y);
+		add_dyn_polygon(&polygon);
+	}
+}
