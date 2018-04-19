4d032f9482d770fcfdae17a4453458ce8a246763
Gilles DOFFE <g.doffe@gmail.com>
2018-04-19 11:36:50 +0200

    robotics/avoidance: support dynamic obstacles
    * handle one dynamic obstacle
    * manage border case when robot is inside an obstacle
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- robotics/include/obstacle.h
+++ robotics/include/obstacle.h
@@ -1,6 +1,10 @@
 #ifndef OBSTACLE_H_
 #define OBSTACLE_H_
 
+#define OBSTACLE_DYN_LENGTH 300
+#define OBSTACLE_DYN_SIZE   400
+
 void mach_fixed_obstacles_init(void);
+void add_dyn_obstacle(const pose_t *robot_pose/*, analog_sensor_zone_t zone*/);
 
 #endif /* OBSTACLE_H_ */
