8cac9f6f51fa3895129dcaf13198dac816439ac0
Gilles DOFFE <g.doffe@gmail.com>
2018-05-05 15:04:18 +0200

    robotics/avoidance: add borders
    Add borders to avoid going on a point outside of field
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- robotics/include/obstacle.h
+++ robotics/include/obstacle.h
@@ -4,6 +4,7 @@
 #define OBSTACLE_DYN_LENGTH 300
 #define OBSTACLE_DYN_SIZE   400
 
+void borders_init(polygon_t *polygon);
 void mach_fixed_obstacles_init(void);
 void add_dyn_obstacle(const pose_t *robot_pose/*, analog_sensor_zone_t zone*/);
 
