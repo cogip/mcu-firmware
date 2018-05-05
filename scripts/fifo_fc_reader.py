85c6721eb6bc2ab28cfaf569b8c14998e6ba4ba1
Gilles DOFFE <g.doffe@gmail.com>
2018-05-05 13:25:09 +0200

    scripts: add dynamic obstacle
    Add one dynamic obstacle for avoidance simulation
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- scripts/fifo_fc_reader.py
+++ scripts/fifo_fc_reader.py
@@ -5,11 +5,13 @@ import os
 import sys
 import time
 import FreeCAD
+import subprocess
 from threading import Thread
 from PySide import QtCore
 
 path = "/tmp/fifo_fc"
 stop = 0
+storing = False
 
 class Reader(QtCore.QObject):
     instance = None
@@ -28,6 +30,7 @@ class Reader(QtCore.QObject):
         self.doc = None
         self.field = None
         self.box = None
+        self.obs = None
         self.thread_reader = None
         self.timer = None
 
@@ -39,15 +42,18 @@ class Reader(QtCore.QObject):
                 App.loadFile(u"$HOME/Bureau/test.fcstd")
             except:
                 print "File already opened."
-        self.doc = App.getDocument("test")
+        self.doc = App.getDocument("simulation")
         App.ActiveDocument = self.doc
         Gui.ActiveDocument = self.doc
         self.view = Gui.activeDocument().activeView()
 
         self.box = App.ActiveDocument.getObjectsByLabel("Robot")[0]
+        self.obs = App.ActiveDocument.getObjectsByLabel("Obstacle")[0]
 
         self.doc.recompute()
         Gui.activeDocument().activeView().viewAxonometric()
+
+        self.obs.ViewObject.Visibility = True
 
         if self.thread_reader is None:
             self.thread_reader = Thread(target=self._run)
@@ -60,6 +66,10 @@ class Reader(QtCore.QObject):
         self.y = self.box.Placement.Base.y
         self.O = self.box.Placement.Rotation.Angle
 
+        self.obs_x = self.obs.Placement.Base.x
+        self.obs_y = self.obs.Placement.Base.y
+        self.obs_O = self.obs.Placement.Rotation.Angle
+
         if self.timer is None:
             self.timer = QtCore.QTimer()
             QtCore.QObject.connect(self.timer, QtCore.SIGNAL("timeout()"), self._parser)
@@ -71,6 +81,8 @@ class Reader(QtCore.QObject):
     def _parser(self):
         self.box.Placement.Base = FreeCAD.Vector(self.x,self.y,10)
         self.box.Placement.Rotation = FreeCAD.Rotation(self.O, 0, 0)
+        self.obs.Placement.Base = FreeCAD.Vector(self.obs_x,self.obs_y,10)
+        self.obs.Placement.Rotation = FreeCAD.Rotation(self.obs_O, 0, 0)
 
     def _run(self):
         global path
@@ -90,12 +102,19 @@ class Reader(QtCore.QObject):
             l +=c
             if c == '\n':
                 print l,
-                if l.startswith('>>>>') or l == '' :
+                if l == '' :
                     break
-                if not l.startswith("Game time") and not l.startswith("pose reached"):
-                    self.x=int(l.split(',')[3])
-                    self.y=int(l.split(',')[4])
-                    self.O=int(l.split(',')[5])
+                if l.startswith("@c@"):
+                    self.x=int(l.split(',')[4])
+                    self.y=int(l.split(',')[5])
+                    self.O=int(l.split(',')[6])
+                elif l.startswith("@o@"):
+                    self.obs.ViewObject.Visibility = True
+                    self.obs_x=(int(l.split(',')[1]) + int(l.split(',')[5])) /2
+                    self.obs_y=(int(l.split(',')[2]) + int(l.split(',')[6])) /2
+                    self.obs_O=int(l.split(',')[9])
+                    print self.obs_x
+                    print self.obs_y
                 l = ''
             if c == '':
                 break
@@ -107,9 +126,8 @@ class Reader(QtCore.QObject):
     def stop(self):
         self.timer.stop()
 try:
-    reader
+	reader
 except NameError:
-    reader = None
+	reader = None
 reader = Reader()
 reader.myrun()
-
