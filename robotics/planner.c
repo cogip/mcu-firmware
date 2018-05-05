b65dceaa2a85cfdbfd44f626f83e5e26e285f848
Yannick Gicquel <ygicquel@gmail.com>
2018-05-05 08:59:51 +0200

    planner: fix broken calibration
    to avoid automatic start when entering in the menu
    
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- robotics/planner.c
+++ robotics/planner.c
@@ -94,9 +94,8 @@ static int trajectory_get_route_update(const pose_t *robot_pose, pose_t *pose_to
 			{
 				if (path->poses[path->current_pose_idx].act)
 					path->poses[path->current_pose_idx].act();
+				increment_current_pose_idx();
 			}
-
-			increment_current_pose_idx();
 			robot_pose_tmp = pose_reached;
 			need_update = 1;
 		}
