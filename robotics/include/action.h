d065c3588abcdab34121d3564ad8d742178f1d07
Yannick Gicquel <ygicquel@gmail.com>
2018-05-07 16:31:49 +0200

    robotics/action: update to mixed_color identifier
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- robotics/include/action.h
+++ robotics/include/action.h
@@ -8,8 +8,8 @@ void action_init(void); // call at boot time once
 
 void act_catch_same_color_water(void);
 void act_launch_same_color_water(void);
-void act_catch_interleaved_water(void);
-void act_launch_interleaved_water(void);
+void act_catch_mixed_water(void);
+void act_launch_mixed_water(void);
 void act_drop_recycled_water(void);
 void act_open_bee_pusher(void);
 void act_close_bee_pusher(void);
