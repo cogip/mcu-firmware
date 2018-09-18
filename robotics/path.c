3c485003a62f68ff1e899ab3e3cfbb9490bcfa44
Gilles DOFFE <g.doffe@gmail.com>
2018-09-18 16:34:24 +0200

    robotics: cortex 2018 final parameters
    Set last parameters of Cortex 2018 robot.
    This parameters have been tested in the Labo Cesson just after the 2018
    french robotic cup.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- robotics/path.c
+++ robotics/path.c
@@ -27,7 +27,7 @@ static path_pose_t poses[] = {
 
 	 /* ********************************************************* */
 	 /* POSE_INITIAL */
-	{	.pos = {.x = -1500 +160 +10 +22, .y = 160 +22 +1, .O = 90, },
+	{	.pos = {.x = -1500 +160 +10, .y = 150 +30, .O = 90, },
 		.allow_reverse = TRUE,
 		.max_speed = MAX_SPEED,
 		.act = NULL, },
@@ -39,58 +39,113 @@ static path_pose_t poses[] = {
 		.act = NULL, },*/
 
 	/* Goes to same color water distributor and start catching action */
-	{	.pos = {.x = -1500 +160 +10 +22, .y = 865, .O = 90, },
+	{	.pos = {.x = -1500 +160 +10, .y = 840, .O = 90, },
 		.allow_reverse = TRUE,
-		.max_speed = MAX_SPEED/2,
-		.act = act_launch_same_color_water, }, //callback : act_catch_same_color_water
+		.max_speed = 50,
+		.act = act_catch_same_color_water, }, //callback : act_catch_same_color_water
 
 	/* Launch water into water tower */
-	{	.pos = {.x = -1500 +160 +10 +22, .y = 865, .O = 90, },
+	{	.pos = {.x = -1500 +160 +10, .y = 840 +500, .O = 90, },
 		.allow_reverse = TRUE,
 		.max_speed = MAX_SPEED,
 		.act = act_launch_same_color_water, }, //callback : act_launch_same_color_water
 
+	/* Goes to position to deploy arm for the bee (temp to avoid cube) */
+	{	.pos = {.x = -1500 +700, .y = 840 +500, .O = 90, },
+		.allow_reverse = TRUE,
+		.max_speed = MAX_SPEED,
+		.act = NULL, }, //callback : bras abeille en position de poussée
+
 	/* Goes to position to deploy arm for the bee */
-	{	.pos = {.x = -1500 +160 +100 +22, .y = 2000 -160 -40, .O = 90, },
+	{	.pos = {.x = -1500 +160 +100, .y = 2000 -160 -40, .O = 90, },
 		.allow_reverse = TRUE,
 		.max_speed = MAX_SPEED,
 		.act = act_open_bee_pusher, }, //callback : bras abeille en position de poussée
 
 	/* Goes to position to push the bee */
-	{	.pos = {.x = -1500 +160 +100 +22 +200, .y = 2000 -160 -40, .O = 0, },
-		.allow_reverse = TRUE,
+	{	.pos = {.x = -1500 +160 +100 +150, .y = 2000 -160 -40, .O = 0, },
+		.allow_reverse = FALSE,
 		.max_speed = MAX_SPEED,
-		.act = NULL, }, //callback : bras abeille en position de poussée
+		.act = act_open_bee_pusher, }, //callback : bras abeille en position de poussée
 
 	/* Goes to position to close arm for the bee */
-	{	.pos = {.x = -1500 +160 +100 +22 +200, .y = 2000 -160 -40, .O = 0, },
+	{	.pos = {.x = -1500 +160 +100 +150, .y = 2000 -160 -40, .O = 0, },
 		.allow_reverse = TRUE,
 		.max_speed = MAX_SPEED,
 		.act = act_close_bee_pusher, }, //callback : bras abeille en position initiale
 
-	/* Intermediate point to domotic pannel interruptor */
-	{	.pos = {.x = -770, .y = 160 + 22 + 1, .O = 0, },
+	/* Goes to position to deploy arm for the bee (temp to avoid cube) */
+	{	.pos = {.x = -1500 +300, .y = 1200, .O = 0, },
 		.allow_reverse = TRUE,
 		.max_speed = MAX_SPEED,
-		.act = NULL, },
+		.act = NULL, }, //callback : bras abeille en position de poussée
+
+	/* Intermediate point to domotic pannel interruptor */
+	{	.pos = {.x = -370, .y = 1000, .O = 90, },
+		.allow_reverse = TRUE,
+		.max_speed = MAX_SPEED,
+		.act = act_close_bee_pusher, },
 
 	/* Goes to domotic pannel interruptor */
-	{	.pos = {.x = -370, .y = 160 + 22 + 1, .O = 0, },
+	{	.pos = {.x = -370, .y = 150 +1, .O = 90, },
 		.allow_reverse = TRUE,
 		.max_speed = MAX_SPEED,
-		.act = NULL, },
+		.act = act_close_bee_pusher, },
 
-	/* Activate interruptor by rotation */
-	{	.pos = {.x = -370, .y = 160 + 22, .O = 180, },
+	/* Intermediate point to domotic pannel interruptor */
+	{	.pos = {.x = -370, .y = 300, .O = 90, },
 		.allow_reverse = TRUE,
 		.max_speed = MAX_SPEED,
-		.act = NULL, },
+		.act = act_close_bee_pusher, },
 
 	/* Going to mixed color water distributor */
-	{	.pos = {.x = 1250, .y = 2000 -160 -100, .O = 180, },
+	{	.pos = {.x = 1250, .y = 2000 -160 -20, .O = 180, },
 		.allow_reverse = TRUE,
 		.max_speed = MAX_SPEED,
-		.act = NULL, },
+		.act = act_close_bee_pusher, },
+
+	/* Goes to same color water distributor and start catching action */
+	{	.pos = {.x = 890, .y = 2000 -160 -10, .O = 180, },
+		.allow_reverse = FALSE,
+		.max_speed = MAX_SPEED/2,
+		.act = act_catch_same_color_water, }, //callback : act_catch_same_color_water
+
+	/* Goes to position to launch mixed water into tower (temp to avoid cubes) */
+	{	.pos = {.x = 0, .y = 1000, .O = 0, },
+		.allow_reverse = TRUE,
+		.max_speed = MAX_SPEED,
+		.act = NULL, }, //callback : bras abeille en position de poussée
+
+	/* Launch water into water tower */
+	{	.pos = {.x = -1500 +160 +190, .y = 840 +500, .O = 85, },
+		.allow_reverse = TRUE,
+		.max_speed = MAX_SPEED,
+		.act = act_launch_mixed_water, }, //callback : act_launch_same_color_water
+
+	/* Launch water into water tower */
+	{	.pos = {.x = -370, .y = 1540, .O = 90, },
+		.allow_reverse = TRUE,
+		.max_speed = MAX_SPEED,
+		.act = act_drop_recycled_water, }, //callback : act_launch_same_color_water
+
+	/* Goes to cube */
+	{	.pos = {.x = -1500 +850, .y = 1500, .O = 90, },
+		.allow_reverse = TRUE,
+		.max_speed = MAX_SPEED,
+		.act = act_close_bee_pusher, },
+
+	/* Push cube */
+	{	.pos = {.x = -1500 +850, .y = 360, .O = 90, },
+		.allow_reverse = TRUE,
+		.max_speed = MAX_SPEED,
+		.act = act_close_bee_pusher, },
+
+	/* Activate interruptor by rotation */
+	/*{	.pos = {.x = -370, .y = 160 +10, .O = 180, },
+		.allow_reverse = FALSE,
+		.max_speed = MAX_SPEED,
+		.act = NULL, },*/
+
 
 	/*{ .pos = {.x = 500, .y = 500, .O = 0, }, .allow_reverse = TRUE, .act = NULL, },
 	{ .pos = {.x = 500, .y = -500, .O = 0, }, .allow_reverse = TRUE, .act = NULL, },
@@ -100,7 +155,7 @@ static path_pose_t poses[] = {
 path_t robot_path = {
 	/* static cfg */
 	.play_in_loop = FALSE,
-	.nb_pose = 9, //10
+	.nb_pose = 18, //10
 	.poses = poses,
 };
 
