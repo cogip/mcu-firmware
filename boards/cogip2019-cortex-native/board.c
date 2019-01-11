f7dd83e6d6385cf9d38baa0ae0ed0c981f2f0c13
Gilles DOFFE <g.doffe@gmail.com>
2019-01-11 13:48:50 +0100

    boards: add cogip2019-cortex-native board
    This board is a copy of RIOT native board and is used to simulate
    cogip2019-cortex board.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

---
+++ boards/cogip2019-cortex-native/board.c
@@ -0,0 +1,70 @@
+/**
+ * Native Board board implementation
+ *
+ * Copyright (C) 2018 Gilles DOFFE <g.doffe@gmail.com>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ *
+ * @ingroup boards_cogip2019_cortex_native
+ * @{
+ * @file
+ * @author  Gilles DOFFE <g.doffe@gmail.com>
+ * @}
+ */
+#include <inttypes.h>
+#include <stdio.h>
+
+/* RIOT includes */
+#include <board.h>
+#include <log.h>
+
+#ifdef QDEC_NUMOF
+
+#define SIMU_ENC_BUFSIZE    3
+extern int32_t qdecs_value[QDEC_NUMOF];
+
+void native_motor_driver_qdec_simulation(
+    const motor_driver_t motor_driver, uint8_t motor_id,
+    int32_t pwm_duty_cycle)
+{
+    static int16_t simu_motor_encoder[QDEC_NUMOF][SIMU_ENC_BUFSIZE] = {0,};
+
+    uint32_t i = 0, id = 0;
+    int32_t s = 0;
+
+    for (i = 0; i < motor_driver; i++) {
+        const motor_driver_config_t motor_driver_conf =
+            motor_driver_config[motor_driver];
+        id += motor_driver_conf.nb_motors;
+    }
+    id += motor_id;
+
+    if (id < QDEC_NUMOF) {
+
+        for (i = 0; i < SIMU_ENC_BUFSIZE - 1; i++) {
+            s += simu_motor_encoder[id][i];
+            simu_motor_encoder[id][i] = simu_motor_encoder[id][i+1];
+        }
+
+        s = (s + simu_motor_encoder[id][i] + pwm_duty_cycle) / (SIMU_ENC_BUFSIZE + 1);
+        simu_motor_encoder[id][i] = s;
+        qdecs_value[id] = s;
+
+        LOG_DEBUG("MOTOR-DRIVER=%u"             \
+            "    MOTOR_ID = %u"                 \
+            "    PWM_VALUE = %d"                \
+            "    QDEC_ID = %"PRIu32""           \
+            "    QDEC_AVERAGE = %d\n",          \
+            motor_driver, motor_id, pwm_duty_cycle, id, s);
+    }
+    else {
+        LOG_ERROR("MOTOR-DRIVER=%u"             \
+            "    MOTOR_ID = %u"                 \
+            "    no QDEC device associated",    \
+            motor_driver, motor_id);
+    }
+}
+
+#endif /* QDEC_NUMOF */
---
+++ boards/cogip2019-cortex-native/board_init.c
@@ -0,0 +1,45 @@
+/**
+ * Native Board board_init implementation
+ *
+ * Copyright (C) 2014 Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ *
+ * @ingroup boards_cogip2019_cortex_native
+ * @{
+ * @file
+ * @author  Gilles DOFFE <g.doffe@gmail.com>
+ * @}
+ */
+#include <stdio.h>
+#include "board.h"
+
+#include "board_internal.h"
+
+#ifdef MODULE_MTD
+#include "mtd_native.h"
+#endif
+
+/**
+ * Nothing to initialize at the moment.
+ */
+void board_init(void)
+{
+    puts("COGIP 2019 native board initialized.");
+}
+
+#ifdef MODULE_MTD
+static mtd_native_dev_t mtd0_dev = {
+    .dev = {
+        .driver = &native_flash_driver,
+        .sector_count = MTD_SECTOR_NUM,
+        .pages_per_sector = MTD_SECTOR_SIZE / MTD_PAGE_SIZE,
+        .page_size = MTD_PAGE_SIZE,
+    },
+    .fname = MTD_NATIVE_FILENAME,
+};
+
+mtd_dev_t *mtd0 = (mtd_dev_t *)&mtd0_dev;
+#endif
