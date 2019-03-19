bf000ad45e319d00341931f84015f7483a2c472b
Gilles DOFFE <g.doffe@gmail.com>
2019-03-19 03:00:32 +0100

    drivers: add sd21 driver
    This reverts partially commit 3536b160d16732252ca9846b02e271ff9281fd06.
    
    SD21 is a board than can driver 21 servomotors and that is drived trough an
    I2C bus.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

---
+++ drivers/sd21/sd21.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+
+#include "platform.h"
+#include "sd21.h"
+#include "xtimer.h"
+
+#define REG_VERSION     64
+#define REG_VOLTAGE     65
+
+/* calibration constants */
+#define CAL_RST 1600
+#define CAL_MIN 600
+#define CAL_MAX 2600
+
+static uint16_t sd21_servo_positions[SD21_NUMOF][SD21_SERVO_NUMOF];
+
+static void sd21_check_config(sd21_t dev, uint8_t servo_id)
+{
+    assert(dev < SD21_NUMOF);
+
+    const sd21_conf_t *sd21 = &sd21_config[dev];
+
+    assert(servo_id < sd21->servos_nb);
+}
+
+static void sd21_send_twi_cmd(sd21_t dev, uint8_t servo_id, uint8_t speed, uint16_t position)
+{
+    const sd21_conf_t *sd21 = &sd21_config[dev];
+
+    assert(servo_id < sd21->servos_nb);
+
+    uint8_t reg = (servo_id) * 3;
+    uint8_t data[2];
+
+    i2c_acquire(sd21->i2c_dev_id);
+
+    data[0] = reg;
+    data[1] = speed;
+    i2c_write_bytes(sd21->i2c_dev_id, sd21->i2c_address, data, 2, 0);
+
+    data[0] = reg + 1;
+    data[1] = (uint8_t) (position & 0x00FF);
+    i2c_write_bytes(sd21->i2c_dev_id, sd21->i2c_address, data, 2, 0);
+
+    data[0] = reg + 2;
+    data[1] = position >> 8;
+    i2c_write_bytes(sd21->i2c_dev_id, sd21->i2c_address, data, 2, 0);
+
+    sd21_servo_positions[dev][servo_id] = position;
+
+    i2c_release(sd21->i2c_dev_id);
+}
+
+void sd21_servo_control(sd21_t dev, uint8_t servo_id, uint8_t speed,
+        uint16_t position)
+{
+    sd21_check_config(dev, servo_id);
+
+    /* Limit position */
+    if (position < SD21_SERVO_POS_MIN)
+        position = SD21_SERVO_POS_MIN;
+    if (position > SD21_SERVO_POS_MAX)
+        position = SD21_SERVO_POS_MAX;
+
+    sd21_send_twi_cmd(dev, servo_id, speed, position);
+}
+
+void sd21_servo_reach_position(sd21_t dev, uint8_t servo_id, uint8_t pos_index)
+{
+    assert(pos_index < SD21_SERVO_POS_NUMOF);
+
+    sd21_check_config(dev, servo_id);
+
+    const sd21_conf_t *sd21 = &sd21_config[dev];
+
+    const sd21_servo_t *servo = &sd21->servos[servo_id];
+
+    sd21_servo_control(dev, servo_id, servo->default_speed,
+            servo->positions[pos_index]);
+}
+
+void sd21_servo_reset_position(sd21_t dev, uint8_t servo_id)
+{
+
+    sd21_check_config(dev, servo_id);
+
+    const sd21_conf_t *sd21 = &sd21_config[dev];
+
+    const sd21_servo_t *servo = &sd21->servos[servo_id];
+
+    assert(servo->default_position < SD21_SERVO_POS_NUMOF);
+
+    sd21_servo_control(dev, servo_id, servo->default_speed,
+            servo->positions[servo->default_position]);
+}
+
+uint16_t sd21_servo_get_position(sd21_t dev, uint8_t servo_id)
+{
+    sd21_check_config(dev, servo_id);
+
+    return sd21_servo_positions[dev][servo_id];
+}
+
+const char* sd21_servo_get_name(sd21_t dev, uint8_t servo_id)
+{
+    sd21_check_config(dev, servo_id);
+
+    const sd21_conf_t *sd21 = &sd21_config[dev];
+
+    const sd21_servo_t *servo = &sd21->servos[servo_id];
+
+    return servo->name;
+}
+
+
+void sd21_init(void)
+{
+    for (sd21_t dev = 0; dev < SD21_NUMOF; dev++) {
+        /* Init I2C */
+        i2c_init(sd21_config[dev].i2c_dev_id);
+
+        /* Wait for I2C to be ready */
+        xtimer_sleep(1);
+
+        /* Close all servomotors */
+        for (uint8_t servo_id = 0; servo_id < sd21_config[dev].servos_nb;
+                servo_id++) {
+                sd21_servo_reset_position(dev, servo_id);
+        }
+    }
+}
