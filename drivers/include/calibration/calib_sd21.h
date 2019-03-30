62e2c28eec27fc9eced10701690e4f4f661a6d1d
Gilles DOFFE <g.doffe@gmail.com>
2019-03-30 19:41:05 +0100

    drivers: sd21: add calibration support
    Add calibration support to SD21.
    It results in a sd21_calib RIOT shell command with the following usage:
            'o'      Opened position
            'c'      Closed position
            'n'      Next servomotor
            'p'      Previous servomotor
            'q'      Quit calibration
            'r'      Reset to center position
            '+'      Add 20 microseconds to current position
            '+'      Substract 20 microseconds to current position
            '0..1'   Switch to predefined positions (between 0 and 9)
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

---
+++ drivers/include/calibration/calib_sd21.h
@@ -0,0 +1,18 @@
+#ifndef CALIB_SD21_H_
+#define CALIB_SD21_H_
+
+#include "sd21.h"
+
+/**
+ * @brief SD21 duty cycle step
+ */
+#define SD21_SERVO_POS_STEP    20
+
+/**
+ * @brief Initialize SD21 calibration, mainly adds commands to RIOT shell.
+ *
+ * @return
+ */
+void sd21_calib_init(void);
+
+#endif /* CALIB_SD21_H_ */
