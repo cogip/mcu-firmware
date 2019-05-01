6b8a0b52498a040e88fbbeb7058bd9cfef5727f5
Gilles DOFFE <g.doffe@gmail.com>
2019-05-01 09:51:08 +0200

    boards: cogip2019-cortex: fix UART description
    UART3 will be used to communicate with a LIDAR.
    Replace UART6 by UART3 and set it up accordingly.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- boards/cogip2019-cortex/include/periph_conf.h
+++ boards/cogip2019-cortex/include/periph_conf.h
@@ -109,14 +109,14 @@ static const uart_conf_t uart_config[] = {
 #endif
     },
     {
-        .dev = USART6,
-        .rcc_mask = RCC_APB2ENR_USART6EN,
-        .rx_pin = GPIO_PIN(PORT_A, 12),
-        .tx_pin = GPIO_PIN(PORT_A, 11),
+        .dev = USART3,
+        .rcc_mask = RCC_APB1ENR_USART3EN,
+        .rx_pin = GPIO_PIN(PORT_C, 5),
+        .tx_pin = GPIO_PIN(PORT_C, 10),
         .rx_af = GPIO_AF8,
         .tx_af = GPIO_AF8,
         .bus = APB2,
-        .irqn = USART6_IRQn,
+        .irqn = USART3_IRQn,
 #ifdef UART_USE_DMA
         .dma_stream = 6,
         .dma_chan = 4
