0521e9331e967fd33870765ac01389c88d8b5d6b
Gilles DOFFE <g.doffe@gmail.com>
2019-05-01 19:03:38 +0200

    drivers: add PCA9548 I2C switch driver
    The PCA9548 is an 8 channels I2C switch. It allows to drive up to 8 I2C
    devices that shares the same I2C address.
    The PCA9548 has a default address of 0x70 defined on 7 bits.
    Using three hardware pins, I2C address can be steup between 0x70 and 0x77.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

---
+++ drivers/include/calibration/calib_pca9548.h
@@ -0,0 +1,8 @@
+#ifndef CALIB_PCA9548_H_
+#define CALIB_PCA9548_H_
+
+#include "pca9548.h"
+
+void pca9548_calib_init(void);
+
+#endif /* CALIB_PCA9548_H_ */
---
+++ drivers/pca9548/pca9548.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#include "platform.h"
+#include "pca9548.h"
+#include "xtimer.h"
+
+static uint8_t pca9548_current_channel[PCA9548_NUMOF];
+
+void pca9548_set_current_channel(pca9548_t dev, uint8_t channel)
+{
+    assert(dev < PCA9548_NUMOF);
+
+    const pca9548_conf_t *pca9548 = &pca9548_config[dev];
+
+    assert(channel < pca9548->channel_numof);
+
+    i2c_acquire(pca9548->i2c_dev_id);
+
+    int err = 1;
+    err = i2c_write_byte(pca9548->i2c_dev_id, pca9548->i2c_address, 1 << channel, 0);
+
+    i2c_release(pca9548->i2c_dev_id);
+
+    if (!err) {
+        pca9548_current_channel[dev] = channel;
+    }
+}
+
+uint8_t pca9548_get_current_channel(pca9548_t dev)
+{
+    assert(dev < PCA9548_NUMOF);
+
+    return pca9548_current_channel[dev];
+}
+
+void pca9548_init(void)
+{
+    for (pca9548_t dev = 0; dev < PCA9548_NUMOF; dev++) {
+        const pca9548_conf_t *pca9548 = &pca9548_config[dev];
+
+        assert(pca9548->channel_numof <= PCA9548_CHANNEL_MAX);
+
+        pca9548_set_current_channel(dev, 0);
+    }
+}
