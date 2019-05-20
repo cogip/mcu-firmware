c92f4b5eaebc7042e26f69ce3485e8dea97106af
Gilles DOFFE <g.doffe@gmail.com>
2019-05-20 13:58:41 +0200

    robotics: obstacle: display sensor device id
    When adding a dynamic obstacle from a sensor, display the sensor device id
    to identify the sensor.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- robotics/include/obstacle.h
+++ robotics/include/obstacle.h
@@ -6,7 +6,8 @@
 
 void borders_init(polygon_t *polygon);
 void mach_fixed_obstacles_init(void);
-int8_t add_dyn_obstacle(const pose_t *robot_pose,
+int8_t add_dyn_obstacle(const uint16_t dev,
+                        const pose_t *robot_pose,
                         double angle_offset,
                         double distance_offset,
                         double dist);
