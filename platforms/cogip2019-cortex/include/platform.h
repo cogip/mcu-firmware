a68e599da58b7eee023ac98fa00a42628af719ff
Yannick Gicquel <ygicquel@gmail.com>
2019-05-26 16:03:49 +0200

    platforms: cogip2019-cortex: tune back lift bottom pos for back ramp shift compensation
    Back ramp is next to back lifts by approx. 5mm. Puck can block.
    By sucking puck at higher altitude point, we can compensate the storage
    in the back ramp.
    
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- platforms/cogip2019-cortex/include/platform.h
+++ platforms/cogip2019-cortex/include/platform.h
@@ -404,7 +404,7 @@ static const sd21_conf_t sd21_config[] = {
             {
                 .positions = {
                     1000,   /* Top position */
-                    1800,   /* Bottom position */
+                    1700,   /* Bottom position */
                     1500,   /* Default */
                 },
                 .default_position = 1,
@@ -415,7 +415,7 @@ static const sd21_conf_t sd21_config[] = {
             {
                 .positions = {
                     1100,   /* Top position */
-                    1975,   /* Bottom position */
+                    1900,   /* Bottom position */
                     1500,   /* Default */
                 },
                 .default_position = 1,
@@ -426,7 +426,7 @@ static const sd21_conf_t sd21_config[] = {
             {
                 .positions = {
                     2125,   /* Top position */
-                    1100,   /* Bottom position */
+                    1250,   /* Bottom position */
                     1500,   /* Default */
                 },
                 .default_position = 1,
