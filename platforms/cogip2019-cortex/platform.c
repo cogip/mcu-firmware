0fe2a3fda4f0dae48a46e9bccff3c8356ef9277a
Yannick Gicquel <ygicquel@gmail.com>
2019-05-26 21:56:50 +0200

    platforms: cogip2019-cortex: check color in left/right dependants actions
    Functions identifier to be updated later on.
    
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- platforms/cogip2019-cortex/platform.c
+++ platforms/cogip2019-cortex/platform.c
@@ -171,9 +171,12 @@ void pf_back_cup_ramp(void)
 
 void pf_front_ramp_right_drop(void)
 {
+    uint8_t is_camp_left = pf_is_camp_left();
+
     sd21_servo_reach_position(PF_SERVO_F_RAMP_BLOCK, PF_SERVO_STATE_RAMP_OPEN);
     sd21_servo_reach_position(PF_SERVO_FR_RAMP_DISP, PF_SERVO_STATE_RAMP_OPEN);
-    sd21_servo_reach_position(PF_SERVO_F_RAMP, PF_SERVO_STATE_RAMP_RIGHT);
+    sd21_servo_reach_position(PF_SERVO_F_RAMP,
+                              is_camp_left ? PF_SERVO_STATE_RAMP_LEFT : PF_SERVO_STATE_RAMP_RIGHT);
     xtimer_usleep(3000 * US_PER_MS);
 
     pf_front_ramp_reset();
@@ -192,9 +195,12 @@ void pf_front_ramp_reset(void)
 
 void pf_back_ramp_left_drop(void)
 {
+    uint8_t is_camp_left = pf_is_camp_left();
+
     sd21_servo_reach_position(PF_SERVO_B_RAMP_BLOCK, PF_SERVO_STATE_RAMP_OPEN);
     sd21_servo_reach_position(PF_SERVO_BL_RAMP_DISP, PF_SERVO_STATE_RAMP_OPEN);
-    sd21_servo_reach_position(PF_SERVO_B_RAMP, PF_SERVO_STATE_RAMP_LEFT);
+    sd21_servo_reach_position(PF_SERVO_B_RAMP,
+                              is_camp_left ? PF_SERVO_STATE_RAMP_RIGHT : PF_SERVO_STATE_RAMP_LEFT);
     xtimer_usleep(3000 * US_PER_MS);
 
     pf_back_ramp_reset();
