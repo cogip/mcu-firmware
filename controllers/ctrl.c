140e332147bd29d137a89a26e88e2dacdb4f088b
Yannick Gicquel <ygicquel@gmail.com>
2019-10-24 07:20:58 +0200

    controllers: store speed_order instead of pointer in instance
    This store the speed_order inside the controller instance, thus keeping
    its value consistent for the overall object, as other instances
    variables. This fix issue #8.
    
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- controllers/ctrl.c
+++ controllers/ctrl.c
@@ -102,14 +102,14 @@ inline void ctrl_set_speed_order(ctrl_t* ctrl, polar_t* speed_order)
 
     irq_disable();
 
-    ctrl->control.speed_order = speed_order;
+    ctrl->control.speed_order = *speed_order;
 
     irq_enable();
 }
 
 inline polar_t* ctrl_get_speed_order(ctrl_t* ctrl)
 {
-    return ctrl->control.speed_order;
+    return &ctrl->control.speed_order;
 }
 
 void ctrl_set_mode(ctrl_t* ctrl, ctrl_mode_t new_mode)
