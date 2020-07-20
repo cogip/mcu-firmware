7af052c17f4e103490a4e8649dab86b325a94e1d
Eric Courtois <eric.courtois@gmail.com>
2020-07-20 20:06:17 +0200

    calib: pca9548sd21: use standard shell_commands
    Replace custom sub-menus by RIOT shell commands
    No new commands added.
    
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- drivers/pca9548/calibration/calib_pca9548.c
+++ drivers/pca9548/calibration/calib_pca9548.c
@@ -7,22 +7,94 @@
 #include "fmt.h"
 
 /* Project includes */
+#define ENABLE_DEBUG        (0)
+#include "debug.h"
 #include "board.h"
 #include "pca9548.h"
 #include "platform.h"
 #include "calibration/calib_pca9548.h"
 
-static void pca9548_calib_print_usage(pca9548_t dev)
-{
-    printf(">>> Entering calibration for pca9548 %u\n\n",
-            dev);
+/* Shell command array */
+static shell_command_linked_t pca9548_shell_commands;
+
+/* Board and servo ids */
+static pca9548_t dev = 0;
+static uint8_t channel_id = 0;
 
 #ifdef PCA9548_CALIB_CB
-    puts("\t'a'\t Run defined callback");
+static int pca9548_cmd_defined_callback_cb(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    PCA9548_CALIB_CB(dev);
+
+    return EXIT_SUCCESS;
+}
 #endif /* PCA9548_CALIB_CB */
-    puts("\t'n'\t Next channel");
-    puts("\t'p'\t Previous channel");
-    puts("\t'r'\t Refresh");
+
+static int pca9548_cmd_next_channel_cb(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    channel_id = (channel_id >= pca9548_config[dev].channel_numof - 1) ?
+        pca9548_config[dev].channel_numof - 1 : channel_id + 1;
+
+    return EXIT_SUCCESS;
+}
+
+static int pca9548_cmd_previous_channel_cb(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    channel_id = (channel_id <= 0) ? 0 : channel_id - 1;
+
+    return EXIT_SUCCESS;
+}
+
+static int pca9548_cmd_refresh_cb(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    channel_id = (channel_id <= 0) ? 0 : channel_id - 1;
+
+    return EXIT_SUCCESS;
+}
+
+static int pca9548_cmd_switch_cb(int argc, char **argv)
+{
+    /* Check arguments */
+    if (argc != 2) {
+        puts("Bad number of arguments!");
+        return EXIT_FAILURE;
+    }
+
+    uint8_t channel_selected = (uint8_t)atoi(argv[1]);
+
+    switch (channel_selected)
+    {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+            if (channel_selected < pca9548_config[dev].channel_numof) {
+                channel_id = channel_selected;
+            }
+            break;
+        default:
+            break;
+    }
+
+    return EXIT_SUCCESS;
 }
 
 static int pca9548_calib_cmd(int argc, char **argv)
@@ -32,71 +104,39 @@ static int pca9548_calib_cmd(int argc, char **argv)
 
     /* Check arguments */
     if (argc != 2) {
-        puts("Bad arguments number !");
+        puts("Bad number of arguments!");
         ret = -1;
         goto pca9548_calib_servo_cmd_err;
     }
 
     /* Get board and servo ids */
-    pca9548_t dev = atoi(argv[1]);
-    uint8_t channel_id = pca9548_get_current_channel(dev);
+    dev = atoi(argv[1]);
+    channel_id = pca9548_get_current_channel(dev);
 
-    /* Display usage */
-    pca9548_calib_print_usage(dev);
+    pf_init_shell_commands(&pca9548_shell_commands);
 
-    puts("");
-    puts("=======================");
-    puts("");
+    pf_add_shell_command(&pca9548_shell_commands, &cmd_exit_shell);
 
-    /* Key pressed */
-    char c[2];
-
-    while (c[0] != 'q') {
-        printf("Channel number: %u\n", channel_id);
-
-        c[0] = getchar();
-
-        /* useful only for 0 to 7 keys */
-        uint8_t channel_selected = (uint8_t)atoi(c);
-
-        switch(c[0]) {
 #ifdef PCA9548_CALIB_CB
-            /* Calibration callback */
-            case 'a':
-                PCA9548_CALIB_CB(dev);
-                break;
+    shell_command_t pca9548_cmd_defined_callback = {"a", "Run defined callback", pca9548_cmd_defined_callback_cb};
+    pf_add_shell_command(&pca9548_shell_commands, &pln_cmd_defined_callback);
 #endif /* PCA9548_CALIB_CB */
-            /* Next channel */
-            case 'n':
-                channel_id = (channel_id >= pca9548_config[dev].channel_numof - 1) ?
-                    pca9548_config[dev].channel_numof - 1 : channel_id + 1;
-                break;
-            /* Previous channel */
-            case 'p':
-                channel_id = (channel_id <= 0) ? 0 : channel_id - 1;
-                break;
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-                if (channel_selected < pca9548_config[dev].channel_numof)
-                    channel_id = channel_selected;
-                break;
-            default:
-                continue;
-        }
 
-        pca9548_set_current_channel(dev, channel_id);
+    shell_command_t pca9548_cmd_next_channel = {"n", "Next channel", pca9548_cmd_next_channel_cb};
+    pf_add_shell_command(&pca9548_shell_commands, &pca9548_cmd_next_channel);
 
-    }
+    shell_command_t pca9548_cmd_previous_channel = {"p", "Previous channel", pca9548_cmd_previous_channel_cb};
+    pf_add_shell_command(&pca9548_shell_commands, &pca9548_cmd_previous_channel);
 
-    puts("");
+    shell_command_t pca9548_cmd_refresh = {"r", "Refresh", pca9548_cmd_refresh_cb};
+    pf_add_shell_command(&pca9548_shell_commands, &pca9548_cmd_refresh);
 
-    return ret;
+    shell_command_t pca9548_cmd_switch = {"switch", "Switch to channel <n> (n between 0 and 9)", pca9548_cmd_switch_cb};
+    pf_add_shell_command(&pca9548_shell_commands, &pca9548_cmd_switch);
+
+    /* Push new menu */
+    DEBUG("pca9548: Start shell\n");
+    pf_push_shell_commands(&pca9548_shell_commands);
 
 pca9548_calib_servo_cmd_err:
     return ret;
