57b2dd3134fd76c1b785cfefbed88c9cbf3bb9fb
Gilles DOFFE <g.doffe@gmail.com>
2020-08-14 00:20:08 +0200

    applications: introduce simulation configuration
    Applications running in simulation mode are applications running
    on a native CPU architecture.
    Simulation case implies a specific configuration as the system
    is not facing physical constraints.
    Thus configuration properties that could be different between
    real context and simulation context are now splitted.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- applications/cogip2020-cortex/include/app.h
+++ applications/cogip2020-cortex/include/app.h
@@ -1,5 +1,4 @@
-#ifndef APP_H_
-#define APP_H_
+#pragma once
 
 #define SD21_SERVO_NUMOF        12
 #define SD21_SERVO_POS_NUMOF    3
@@ -21,33 +20,6 @@
 
 #define PCA9548_CALIB_CB(x) pf_calib_read_sensors(x)
 
-static const ctrl_quadpid_parameters_t ctrl_quadpid_params = {
-        .linear_speed_pid = {
-            .kp = 150.,
-            .ki = 2,
-            .kd = 0.,
-        },
-        .angular_speed_pid = {
-            .kp = 150.,
-            .ki = 2,
-            .kd = 0.,
-        },
-        .linear_pose_pid = {
-            .kp = 1,
-            .ki = 0.,
-            .kd = 2,
-        },
-        .angular_pose_pid = {
-            .kp = 1,
-            .ki = 0.,
-            .kd = 5,
-        },
-
-        .min_distance_for_angular_switch = 3,   // mm,
-        .min_angle_for_pose_reached = 2,        // deg,
-        .regul = CTRL_REGUL_POSE_DIST,
-};
-
 static const sd21_conf_t sd21_config[] = {
     {   /* SD12 ID0 */
         .i2c_dev_id = 0,
@@ -62,5 +34,3 @@ void app_calib_read_sensors(pca9548_t dev);
 
 void app_init(void);
 void app_init_tasks(void);
-
-#endif /* APP_H_ */
