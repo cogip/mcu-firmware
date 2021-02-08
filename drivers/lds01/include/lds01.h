62809a8785893d462075746fd0a2bd80c49a8abe
Eric Courtois <eric.courtois@gmail.com>
2021-02-08 16:24:55 +0100

    drivers: lds01: change API using id instead of pointers
    A device is now identified by its ID in API functions,
    not by a pointer to its internal structure.
    It makes the header file simpler and restrict access to internal data.
    
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- drivers/lds01/include/lds01.h
+++ drivers/lds01/include/lds01.h
@@ -7,8 +7,8 @@
  */
 
 /**
- * @defgroup    driver_lds01 LDS01 (aka HLS-LFCD2) LiDAR driver
- * @ingroup     drivers_sensors
+ * @defgroup    driver_lds01 LDS01 LiDAR driver
+ * @ingroup     drivers
  * @brief       LDS01 (aka HLS-LFCD2) LiDAR driver
  *
  * The LDS01 (aka HLS-LFCD2) device is a 360° 2D LiDAR distance sensor.
@@ -40,29 +40,6 @@
 extern "C" {
 #endif
 
-/**
- * @brief   LDS01 offset
- */
-typedef struct {
-    uint8_t intensity[2];   /**< intentity bytes */
-    uint8_t distance[2];    /**< distance bytes */
-    uint8_t reserved[2];    /**< reserved bytes */
-} lds01_offset_t;
-
-/**
- * @brief   LDS01 frame
- */
-typedef struct {
-    uint8_t sync;               /**< begin frame byte (0xAF) */
-    uint8_t index;              /**< frame index */
-    uint8_t rpm[2];             /**< rpm (unused)*/
-    lds01_offset_t offsets[6];  /**< frame offsets */
-    uint8_t checksum[2];        /**< frame checksum */
-} lds01_frame_t;
-
-#define LDS01_UART_BAUD (230400U)                   /**< lds01 UART baud rate */
-#define LDS01_FRAME_SIZE (sizeof(lds01_frame_t))    /**< lds01 frame size */
-#define LDS01_BUFFER_SIZE (LDS01_FRAME_SIZE * 4)    /**< size of the ringbuffer used to store raw data from UART */
 #define LDS01_NB_ANGLES (360U)                      /**< number of angles (360°) */
 
 typedef void (*lds01_new_frame_cb_t)(void);         /**< new frame callback signature */
@@ -76,76 +53,66 @@ typedef struct {
 } lds01_params_t;
 
 /**
- * @brief   LDS01 descriptor
+ * @brief   Default LDS01 identifier definition
  */
-typedef struct {
-    lds01_params_t params;                  /**< parameters */
-    bool running;                           /**< set to true if lds01 is running */
-    uint16_t filter;                        /**< distance filter in millimeters */
-    mutex_t data_lock;                      /**< lock protecting data access */
-    uint16_t distances[LDS01_NB_ANGLES];    /**< distance data */
-    uint16_t intensities[LDS01_NB_ANGLES];  /**< intensity data */
-    char rx_mem[LDS01_BUFFER_SIZE];         /**< raw frame buffer */
-    ringbuffer_t rx_buf;                    /**< frame ringbuffer */
-    uint8_t remaining_bytes_in_frame;       /**< number of bytes to read for a full frame */
-} lds01_t;
+typedef unsigned int lds01_t;
 
 /**
  * @brief Initialize LDS01 device
  *
- * @param[out]  lds01   lds01 device
+ * @param[in]   lds01   lds01 device id to initialize
  * @param[in]   params  lds01 parameters
  *
  * @return              0 on success, not 0 otherwise
  */
-int lds01_init(lds01_t *lds01, const lds01_params_t *params);
+int lds01_init(const lds01_t lds01, const lds01_params_t *params);
 
 /**
  * @brief Update data arrays
  *
  * Get the last fame and update corresponding indexes in data arrays
  *
- * @param[in]   lds01       lds01 device
+ * @param[in]   lds01   lds01 device id
  */
-void lds01_update_last_frame(lds01_t *lds01);
+void lds01_update_last_frame(const lds01_t lds01);
 
 /**
  * @brief Start LDS01 device
  *
- * @param[in]   lds01       lds01 device
+ * @param[in]   lds01       lds01 device id
  */
-void lds01_start(lds01_t *lds01);
+void lds01_start(const lds01_t lds01);
 
 /**
  * @brief Stop LDS01 device
  *
- * @param[in]   lds01       lds01 device
+ * @param[in]   lds01       lds01 device id
  */
-void lds01_stop(lds01_t *lds01);
+void lds01_stop(const lds01_t lds01);
 
 /**
  * @brief Set new distance filter value
  *
- * @param[in]   lds01       lds01 device
+ * @param[in]   lds01       lds01 device id
  * @param[in]   new_filter  value of the new filter, in millimeters, 0 means no filter
  */
-void lds01_set_distance_filter(lds01_t *lds01, uint16_t new_filter);
+void lds01_set_distance_filter(const lds01_t lds01, uint16_t new_filter);
 
 /**
  * @brief Get distance data
  *
- * @param[in]   lds01      lds01 device
+ * @param[in]   lds01      lds01 device id
  * @param[out]  distances  array pointer to copy distance data
  */
-void lds01_get_distances(lds01_t *lds01, uint16_t *distances);
+void lds01_get_distances(const lds01_t lds01, uint16_t *distances);
 
 /**
  * @brief Get intentity data
  *
- * @param[in]   lds01        lds01 device
+ * @param[in]   lds01        lds01 device id
  * @param[out]  intensities  array pointer to copy intensity data
  */
-void lds01_get_intensities(lds01_t *lds01, uint16_t *intensities);
+void lds01_get_intensities(const lds01_t lds01, uint16_t *intensities);
 
 #ifdef __cplusplus
 }
--- drivers/lds01/lds01.c
+++ drivers/lds01/lds01.c
@@ -6,44 +6,83 @@
 
 #include <string.h>
 
-#define LDS01_NUM ARRAY_SIZE(lds01_params)  /**< number of used lds01 devices */
-#define LDS01_FRAME_SYNC_BYTE (0xFA)        /**< first byte of a frame */
-#define LDS01_NB_FRAMES (40U)               /**< number of frames to get all angles */
-#define LDS01_NB_ANGLES_BY_FRAME (6U)       /**< number of angles in one frame */
+#define LDS01_NUMOF ARRAY_SIZE(lds01_params)        /**< number of used lds01 devices */
+#define LDS01_FRAME_SYNC_BYTE (0xFA)                /**< first byte of a frame */
+#define LDS01_NB_FRAMES (40U)                       /**< number of frames to get all angles */
+#define LDS01_NB_ANGLES_BY_FRAME (6U)               /**< number of angles in one frame */
+
+#define LDS01_UART_BAUD (230400U)                   /**< lds01 UART baud rate */
+#define LDS01_FRAME_SIZE (sizeof(lds01_frame_t))    /**< lds01 frame size */
+#define LDS01_BUFFER_SIZE (LDS01_FRAME_SIZE * 4)    /**< size of the ringbuffer used to store raw data from UART */
+
+/**
+ * @brief   LDS01 offset
+ */
+typedef struct {
+    uint8_t intensity[2];   /**< intentity bytes */
+    uint8_t distance[2];    /**< distance bytes */
+    uint8_t reserved[2];    /**< reserved bytes */
+} lds01_offset_t;
+
+/**
+ * @brief   LDS01 frame
+ */
+typedef struct {
+    uint8_t sync;               /**< begin frame byte (0xAF) */
+    uint8_t index;              /**< frame index */
+    uint8_t rpm[2];             /**< rpm (unused)*/
+    lds01_offset_t offsets[6];  /**< frame offsets */
+    uint8_t checksum[2];        /**< frame checksum */
+} lds01_frame_t;
+
+/**
+ * @brief   LDS01 descriptor
+ */
+typedef struct {
+    lds01_params_t params;                  /**< parameters */
+    bool running;                           /**< set to true if lds01 is running */
+    uint16_t filter;                        /**< distance filter in millimeters */
+    mutex_t data_lock;                      /**< lock protecting data access */
+    uint16_t distances[LDS01_NB_ANGLES];    /**< distance data */
+    uint16_t intensities[LDS01_NB_ANGLES];  /**< intensity data */
+    char rx_mem[LDS01_BUFFER_SIZE];         /**< raw frame buffer */
+    ringbuffer_t rx_buf;                    /**< frame ringbuffer */
+    uint8_t remaining_bytes_in_frame;       /**< number of bytes to read for a full frame */
+} lds01_dev_t;
 
 /* Allocate memory for the device descriptor */
-lds01_t lds01_devs[LDS01_NUM];
+lds01_dev_t lds01_devs[LDS01_NUMOF];
 
 static void lds01_rx_cb(void *arg, uint8_t data)
 {
-    lds01_t *lds01 = (lds01_t *)arg;
+    lds01_dev_t *lds01_dev = (lds01_dev_t *)arg;
 
-    if (data == LDS01_FRAME_SYNC_BYTE && lds01->remaining_bytes_in_frame == 0) {
-        lds01->remaining_bytes_in_frame = LDS01_FRAME_SIZE;
+    if (data == LDS01_FRAME_SYNC_BYTE && lds01_dev->remaining_bytes_in_frame == 0) {
+        lds01_dev->remaining_bytes_in_frame = LDS01_FRAME_SIZE;
     }
-    if (lds01->remaining_bytes_in_frame == 0) {
+    if (lds01_dev->remaining_bytes_in_frame == 0) {
         return;
     }
 
-    ringbuffer_add_one(&(lds01->rx_buf), data);
-    lds01->remaining_bytes_in_frame--;
+    ringbuffer_add_one(&(lds01_dev->rx_buf), data);
+    lds01_dev->remaining_bytes_in_frame--;
 
-    if (lds01->remaining_bytes_in_frame == 0) {
-        if (lds01->params.new_frame_cb) {
-            lds01->params.new_frame_cb();
+    if (lds01_dev->remaining_bytes_in_frame == 0) {
+        if (lds01_dev->params.new_frame_cb) {
+            lds01_dev->params.new_frame_cb();
         }
         else {
-            ringbuffer_remove(&(lds01->rx_buf), LDS01_FRAME_SIZE);
+            ringbuffer_remove(&(lds01_dev->rx_buf), LDS01_FRAME_SIZE);
         }
     }
 }
 
-static void lds01_reset_data(lds01_t *lds01)
+static void lds01_reset_data(lds01_dev_t *lds01_dev)
 {
-    lds01->remaining_bytes_in_frame = 0;
-    memset((void *)lds01->distances, 0, LDS01_NB_ANGLES * sizeof(uint16_t));
-    memset((void *)lds01->intensities, 0, LDS01_NB_ANGLES * sizeof(uint16_t));
-    ringbuffer_init(&(lds01->rx_buf), lds01->rx_mem, LDS01_BUFFER_SIZE);
+    lds01_dev->remaining_bytes_in_frame = 0;
+    memset((void *)lds01_dev->distances, 0, LDS01_NB_ANGLES * sizeof(uint16_t));
+    memset((void *)lds01_dev->intensities, 0, LDS01_NB_ANGLES * sizeof(uint16_t));
+    ringbuffer_init(&(lds01_dev->rx_buf), lds01_dev->rx_mem, LDS01_BUFFER_SIZE);
 }
 
 static int lds01_is_frame_valid(const lds01_frame_t *frame)
@@ -57,23 +96,27 @@ static int lds01_is_frame_valid(const lds01_frame_t *frame)
     return frame->checksum[0] == 0xFF - sum;
 }
 
-static uint16_t lds01_get_filtered_distance(lds01_t *lds01, uint16_t distance, uint16_t intensity)
+static uint16_t lds01_get_filtered_distance(lds01_dev_t *lds01_dev, uint16_t distance, uint16_t intensity)
 {
-    if (lds01->filter == 0) {
+    if (lds01_dev->filter == 0) {
         return distance;
     }
-    if (intensity == 0 || distance == 0 || distance > lds01->filter) {
-        return lds01->filter;
+    if (intensity == 0 || distance == 0 || distance > lds01_dev->filter) {
+        return lds01_dev->filter;
     }
     return distance;
 }
 
-void lds01_update_last_frame(lds01_t *lds01)
+void lds01_update_last_frame(const lds01_t lds01)
 {
+    assert(lds01 < LDS01_NUMOF);
+
+    lds01_dev_t *lds01_dev = &lds01_devs[lds01];
+
     lds01_frame_t frame;
     uint16_t distances_tmp[LDS01_NB_ANGLES_BY_FRAME];
     uint16_t intensities_tmp[LDS01_NB_ANGLES_BY_FRAME];
-    unsigned bytes_read = ringbuffer_get(&(lds01->rx_buf), (char *)&frame, LDS01_FRAME_SIZE);
+    unsigned bytes_read = ringbuffer_get(&(lds01_dev->rx_buf), (char *)&frame, LDS01_FRAME_SIZE);
 
     if (bytes_read != LDS01_FRAME_SIZE) {
         printf("Read error: %u\n", bytes_read);
@@ -86,31 +129,35 @@ void lds01_update_last_frame(lds01_t *lds01)
     for (unsigned int i = 0; i < LDS01_NB_ANGLES_BY_FRAME; i++) {
         uint16_t distance = (frame.offsets[i].distance[1] << 8) + frame.offsets[i].distance[0];
         uint16_t intensity = (frame.offsets[i].intensity[1] << 8) + frame.offsets[i].intensity[0];
-        distances_tmp[i] = lds01_get_filtered_distance(lds01, distance, intensity);
+        distances_tmp[i] = lds01_get_filtered_distance(lds01_dev, distance, intensity);
         intensities_tmp[i] = intensity;
     }
 
     uint8_t index = frame.index - 0xA0;
 
     /* Update values in arrays given in parameters*/
-    mutex_lock(&lds01->data_lock);
+    mutex_lock(&lds01_dev->data_lock);
     memcpy(
-        (void *)(&lds01->distances[index * LDS01_NB_ANGLES_BY_FRAME]),
+        (void *)(&lds01_dev->distances[index * LDS01_NB_ANGLES_BY_FRAME]),
         (void *)distances_tmp, LDS01_NB_ANGLES_BY_FRAME * sizeof(uint16_t));
     memcpy(
-        (void *)(&lds01->intensities[index * LDS01_NB_ANGLES_BY_FRAME]),
+        (void *)(&lds01_dev->intensities[index * LDS01_NB_ANGLES_BY_FRAME]),
         (void *)intensities_tmp, LDS01_NB_ANGLES_BY_FRAME * sizeof(uint16_t));
-    mutex_unlock(&lds01->data_lock);
+    mutex_unlock(&lds01_dev->data_lock);
 }
 
-int lds01_init(lds01_t *lds01, const lds01_params_t *params)
+int lds01_init(const lds01_t lds01, const lds01_params_t *params)
 {
-    lds01->params = *params;
-    lds01->filter = 0;
-    lds01_reset_data(lds01);
-    mutex_init(&lds01->data_lock);
+    assert(lds01 < LDS01_NUMOF);
 
-    int res = uart_init(lds01->params.uart, LDS01_UART_BAUD, lds01_rx_cb, (void *)lds01);
+    lds01_dev_t *lds01_dev = &lds01_devs[lds01];
+
+    lds01_dev->params = *params;
+    lds01_dev->filter = 0;
+    lds01_reset_data(lds01_dev);
+    mutex_init(&lds01_dev->data_lock);
+
+    int res = uart_init(lds01_dev->params.uart, LDS01_UART_BAUD, lds01_rx_cb, (void *)lds01_dev);
     if (res == UART_NOBAUD) {
         printf("Error: Given baudrate (%u) not possible\n", LDS01_UART_BAUD);
         return 1;
@@ -120,7 +167,7 @@ int lds01_init(lds01_t *lds01, const lds01_params_t *params)
         return 1;
     }
 
-    lds01->running = true; /* force stop on startup */
+    lds01_dev->running = true; /* force stop on startup */
     lds01_stop(lds01);
 
     DEBUG("LDS01 UART initialized\n");
@@ -128,40 +175,60 @@ int lds01_init(lds01_t *lds01, const lds01_params_t *params)
     return 0;
 }
 
-void lds01_start(lds01_t *lds01)
+void lds01_start(const lds01_t lds01)
 {
-    if (lds01->running) {
+    assert(lds01 < LDS01_NUMOF);
+
+    lds01_dev_t *lds01_dev = &lds01_devs[lds01];
+
+    if (lds01_dev->running) {
         return;
     }
-    lds01_reset_data(lds01);
-    lds01->running = true;
-    uart_write(lds01->params.uart, (uint8_t *)"b", 1);
+    lds01_reset_data(lds01_dev);
+    lds01_dev->running = true;
+    uart_write(lds01_dev->params.uart, (uint8_t *)"b", 1);
 }
 
-void lds01_stop(lds01_t *lds01)
+void lds01_stop(const lds01_t lds01)
 {
-    if (!lds01->running) {
+    assert(lds01 < LDS01_NUMOF);
+
+    lds01_dev_t *lds01_dev = &lds01_devs[lds01];
+
+    if (!lds01_dev->running) {
         return;
     }
-    uart_write(lds01->params.uart, (uint8_t *)"e", 1);
-    lds01->running = false;
+    uart_write(lds01_dev->params.uart, (uint8_t *)"e", 1);
+    lds01_dev->running = false;
 }
 
-void lds01_set_distance_filter(lds01_t *lds01, uint16_t new_filter)
+void lds01_set_distance_filter(const lds01_t lds01, uint16_t new_filter)
 {
-    lds01->filter = new_filter;
+    assert(lds01 < LDS01_NUMOF);
+
+    lds01_dev_t *lds01_dev = &lds01_devs[lds01];
+
+    lds01_dev->filter = new_filter;
 }
 
-void lds01_get_distances(lds01_t *lds01, uint16_t *distances)
+void lds01_get_distances(const lds01_t lds01, uint16_t *distances)
 {
-    mutex_lock(&lds01->data_lock);
-    memcpy((void *)distances, (void *)lds01->distances, LDS01_NB_ANGLES * sizeof(uint16_t));
-    mutex_unlock(&lds01->data_lock);
+    assert(lds01 < LDS01_NUMOF);
+
+    lds01_dev_t *lds01_dev = &lds01_devs[lds01];
+
+    mutex_lock(&lds01_dev->data_lock);
+    memcpy((void *)distances, (void *)lds01_dev->distances, LDS01_NB_ANGLES * sizeof(uint16_t));
+    mutex_unlock(&lds01_dev->data_lock);
 }
 
-void lds01_get_intensities(lds01_t *lds01, uint16_t *intensities)
+void lds01_get_intensities(const lds01_t lds01, uint16_t *intensities)
 {
-    mutex_lock(&lds01->data_lock);
-    memcpy((void *)intensities, (void *)lds01->intensities, LDS01_NB_ANGLES * sizeof(uint16_t));
-    mutex_unlock(&lds01->data_lock);
+    assert(lds01 < LDS01_NUMOF);
+
+    lds01_dev_t *lds01_dev = &lds01_devs[lds01];
+
+    mutex_lock(&lds01_dev->data_lock);
+    memcpy((void *)intensities, (void *)lds01_dev->intensities, LDS01_NB_ANGLES * sizeof(uint16_t));
+    mutex_unlock(&lds01_dev->data_lock);
 }
