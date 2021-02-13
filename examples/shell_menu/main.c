ad584959b5d510ea8579c520d19d0d2b15125e8f
Eric Courtois <eric.courtois@gmail.com>
2021-02-13 21:32:04 +0100

    sys: add shell_menu module
    Add a sys directory in mcu-firmware.
    Add a first module shell_menu.
    This module extends the shell module by providing an API
    to declare sub-menus and navigate through them.
    
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

---
+++ examples/shell_menu/main.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+
+#include "shell.h"
+#include "shell_menu.h"
+
+static int cmd_1_1(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    puts("Execute command 1 in sub-menu 1");
+
+    return 0;
+}
+
+static int cmd_1_2(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    puts("Execute command 2 in sub-menu 1");
+
+    return 0;
+}
+
+static int cmd_2_1(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    puts("Execute command 1 in sub-menu 2");
+
+    return 0;
+}
+
+static int cmd_2_2(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    puts("Execute command 2 in sub-menu 2");
+
+    return 0;
+}
+
+static int cmd_1(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    puts("Execute command 1 in main menu");
+
+    return 0;
+}
+
+static int cmd_2(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    puts("Execute command 2 in main menu");
+
+    return 0;
+}
+
+static int cmd_sub_1(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    shell_menu_t menu;
+    const shell_command_t commands[] = {
+        { "cmd_1_1", "Command 1/1", cmd_1_1 },
+        { "cmd_1_2", "Command 1/2", cmd_1_2 },
+        { NULL, NULL, NULL }
+    };
+
+    menu_init(&menu, "Sub-menu 1");
+
+    menu_add_list(&menu, commands);
+
+    menu_enter(&menu);
+
+    return 0;
+}
+
+static int cmd_sub_2(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    shell_menu_t menu;
+    const shell_command_t commands[] = {
+        { "cmd_2_1", "Command 2/1", cmd_2_1 },
+        { "cmd_2_2", "Command 2/2", cmd_2_2 },
+        { NULL, NULL, NULL }
+    };
+
+
+    menu_init(&menu, "Sub-menu 2");
+
+    menu_add_list(&menu, commands);
+
+    menu_enter(&menu);
+
+    return 0;
+}
+
+int main(void)
+{
+    puts("\n== Shell menu example ==");
+
+    /* Create the main menu */
+    shell_menu_t main_menu;
+
+    menu_init(&main_menu, "Main menu");
+
+    const shell_command_t main_menu_commands[] = {
+        { "cmd_1", "Command 1", cmd_1 },
+        { "cmd_2", "Command 2", cmd_2 },
+        { "sub1", "Enter sub-menu 1", cmd_sub_1 },
+        { "sub2", "Enter sub-menu 1", cmd_sub_2 },
+        menu_cmd_null
+    };
+
+    menu_add_list(&main_menu, main_menu_commands);
+
+    /* Push main menu */
+    menu_enter(&main_menu);
+
+    /* Start shell */
+    menu_start();
+
+    return 0;
+}
---
+++ sys/shell_menu/shell_menu.c
@@ -0,0 +1,102 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "shell_menu.h"
+
+const shell_command_t menu_cmd_null = {NULL, NULL, NULL};
+
+shell_menu_t current_menu;
+
+static int _display_json_help(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    printf("{\"name\": \"%s\", \"entries\": [", current_menu.name);
+    shell_command_t *cmd = (shell_command_t*)&current_menu;
+    for (; cmd->name != NULL; cmd++) {
+        if(cmd != (shell_command_t*)&current_menu) {
+            printf(", ");
+        }
+        printf(
+            "{\"cmd\": \"%s\", \"desc\": \"%s\"}",
+            cmd->name,
+            cmd->desc
+        );
+    }
+    printf("]}\n");
+    return EXIT_SUCCESS;
+}
+
+static int _exit_menu(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    menu_exit();
+
+    return EXIT_SUCCESS;
+}
+
+const shell_command_t cmd_exit_menu = {
+    "exit", "Exit current menu",
+    _exit_menu
+};
+
+const shell_command_t cmd_help_json = {
+    "_help_json", "Display available commands in JSON format",
+    _display_json_help
+};
+
+void menu_init(shell_menu_t *menu, const char *name) {
+    menu->name = name;
+    menu->current = menu;
+    menu->previous = NULL;
+
+    for(uint8_t i = 0 ; i < NB_SHELL_COMMANDS ; i++) {
+        menu->shell_commands[i] = menu_cmd_null;
+    }
+
+    menu_add_one(menu, &cmd_exit_menu);
+    menu_add_one(menu, &cmd_help_json);
+}
+
+void menu_add_one(shell_menu_t *menu, const shell_command_t *command)
+{
+    uint8_t command_id = 0;
+
+    for (shell_command_t *cmd = menu->shell_commands ; cmd->name != NULL; cmd++, command_id++) {}
+
+    assert(command_id < NB_SHELL_COMMANDS);
+
+    menu->shell_commands[command_id++] = *command;
+}
+
+void menu_add_list(shell_menu_t *menu, const shell_command_t commands[])
+{
+    for (const shell_command_t *cmd = commands; cmd->name != NULL; cmd++) {
+        menu_add_one(menu, cmd);
+    }
+}
+
+void menu_enter(shell_menu_t *menu) {
+    menu->previous = current_menu.current;
+    memcpy(&current_menu, menu, sizeof(shell_menu_t));
+    printf("Enter shell menu: %s\n", menu->name);
+}
+
+void menu_exit(void) {
+    if (current_menu.previous) {
+        printf("Exit shell menu: %s\n", current_menu.name);
+        memcpy(&current_menu, current_menu.previous, sizeof(shell_menu_t));
+        printf("Enter shell menu: %s\n", current_menu.name);
+    }
+}
+
+void menu_start(void)
+{
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+
+    shell_run((shell_command_t*)&current_menu, line_buf, SHELL_DEFAULT_BUFSIZE);
+}
