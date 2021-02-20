dd2d7e3218d68fe74572214930d357db4b468305
Gilles DOFFE <g.doffe@gmail.com>
2021-02-20 01:07:29 +0100

    platforms: calibration: add pf_motors_test()
    This function is used to test each declared motor and its associated
    encoder.
    It makes the motor turning in clowkwise and counter-clockwise during
    3 seconds for each direction and display the encoder measure made
    with associated quadrature decoder.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- platforms/calibration/calib_platform.c
+++ platforms/calibration/calib_platform.c
@@ -57,6 +57,7 @@ void pf_init_shell_commands(shell_command_linked_t *shell_commands, const char *
     pf_add_shell_command(shell_commands, &cmd_help_json);
     pf_add_shell_command(shell_commands, &cmd_print_state);
     pf_add_shell_command(shell_commands, &cmd_print_dyn_obstacles);
+    pf_add_shell_command(shell_commands, &cmd_motors_test);
 }
 
 void pf_add_shell_command(shell_command_linked_t *shell_commands, const shell_command_t *command)
@@ -153,6 +154,71 @@ int pf_print_state(int argc, char **argv)
     return EXIT_SUCCESS;
 }
 
+int pf_motors_test(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    int nb_motors = 0;
+
+    ctrl_set_mode(ctrl, CTRL_MODE_IDLE);
+
+    for (motor_driver_t i = 0; i < MOTOR_DRIVER_NUMOF; i++) {
+        int nb_motors_tmp = motor_driver_config[i].nb_motors;
+        int pwm_resolution = motor_driver_config[i].pwm_resolution;
+
+        for (int j = 0; j < nb_motors_tmp; j++) {
+
+            int32_t qdec_value = 0;
+            int timeout = 3000;
+
+            printf("### Testing motor %d of motor driver %d\n", j, i);
+
+            /* Reset qdec */
+            qdec_read_and_reset(QDEC_DEV(nb_motors + j));
+
+            /* Forward */
+            puts("    Forward move");
+            motor_set(i, j, pwm_resolution / 4);
+            while (timeout--) {
+                qdec_value += qdec_read_and_reset(QDEC_DEV(nb_motors + j));
+                xtimer_usleep(US_PER_MS);
+            }
+            printf("    qdec value = %ld\n", qdec_value);
+            puts("    Done");
+
+            /* Stop */
+            timeout = 3000;
+            qdec_value = 0;
+            puts("    Stop");
+            motor_set(i, j, 0);
+            xtimer_usleep(3 * US_PER_SEC);
+            puts("    Done");
+
+            /* Backward */
+            puts("    Backward move");
+            motor_set(i, j, -pwm_resolution / 4);
+            while (timeout--) {
+                qdec_value += qdec_read_and_reset(QDEC_DEV(nb_motors + j));
+                xtimer_usleep(US_PER_MS);
+            }
+            printf("    qdec value = %ld\n", qdec_value);
+            puts("    Done");
+
+            /* Stop */
+            puts("    Stop");
+            motor_set(i, j, 0);
+            puts("    Done");
+        }
+
+        nb_motors += nb_motors_tmp;
+    }
+
+    ctrl_set_mode(ctrl, CTRL_MODE_STOP);
+
+    return EXIT_SUCCESS;
+}
+
 shell_command_t cmd_exit_shell = {
     "exit", "Exit planner calibration",
     pf_exit_shell
@@ -166,6 +232,11 @@ shell_command_t cmd_help_json = {
 shell_command_t cmd_print_state = {
     "_state", "Print current state",
     pf_print_state
+};
+
+shell_command_t cmd_motors_test = {
+    "mt", "Test all DC motors",
+    pf_motors_test
 };
 
 shell_command_t cmd_print_dyn_obstacles = {
