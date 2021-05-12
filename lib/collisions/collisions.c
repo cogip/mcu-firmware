db3965f53f25a3dd22d6f0dc857cfc8ecd15aad7
Gilles DOFFE <g.doffe@gmail.com>
2021-05-12 21:06:12 +0200

    lib: add collisions library
    This library regroups all functions related to 2D collisions
    management.
    It regroups functions extracted from avoidance.c and that could
    be reused accross various avoidance algorithms.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

---
+++ lib/collisions/collisions.c
@@ -0,0 +1,191 @@
+/* Standard includes */
+#include <stdbool.h>
+#include <stdio.h>
+#include <math.h>
+
+/* Project includes */
+#include "collisions.h"
+
+double collisions_distance_points(const pose_t *a, const pose_t *b)
+{
+    return sqrt((b->x - a->x) * (b->x - a->x)
+                + (b->y - a->y) * (b->y - a->y));
+}
+
+bool collisions_is_point_in_circle(const pose_t *p, const pose_t *center,
+                                   const uint32_t radius)
+{
+    double d = collisions_distance_points(center, p);
+
+    if (d * d > radius * radius) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+bool collisions_is_point_in_polygon(const pose_t *p, const polygon_t *polygon)
+{
+    for (uint8_t i = 0; i < polygon->count; i++) {
+        pose_t a = polygon->points[i];
+        pose_t b = (i == (polygon->count - 1) ? polygon->points[0] : polygon->points[i + 1]);
+        vector_t ab, ap;
+
+        ab.x = b.x - a.x;
+        ab.y = b.y - a.y;
+        ap.x = p->x - a.x;
+        ap.y = p->y - a.y;
+
+        if (ab.x * ap.y - ab.y * ap.x <= 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool collisions_is_point_in_obstacle(const obstacle_t *obstacle, const pose_t *p)
+{
+    for (uint8_t i = 0; i < 4; i++) {
+        pose_t a = obstacle->points[i];
+        pose_t b = (i == (4 - 1) ? obstacle->points[0] : obstacle->points[i + 1]);
+        vector_t ab, ap;
+
+        ab.x = b.x - a.x;
+        ab.y = b.y - a.y;
+        ap.x = p->x - a.x;
+        ap.y = p->y - a.y;
+
+        if (ab.x * ap.y - ab.y * ap.x <= 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool collisions_is_segment_crossing_line(const pose_t *a, const pose_t *b, const pose_t *c, const pose_t *d)
+{
+    vector_t ac, ad, ab;
+    double det = 0;
+
+    ab.x = b->x - a->x;
+    ab.y = b->y - a->y;
+    ad.x = d->x - a->x;
+    ad.y = d->y - a->y;
+    ac.x = c->x - a->x;
+    ac.y = c->y - a->y;
+
+    det = (ab.x * ad.y - ab.y * ad.x) * (ab.x * ac.y - ab.y * ac.x);
+
+    return (det < 0);
+}
+
+bool collisions_is_segment_crossing_segment(const pose_t *a, const pose_t *b,
+                                            const pose_t *c, const pose_t *d)
+{
+    if (!collisions_is_segment_crossing_line(a, b, c, d)) {
+        return false;
+    }
+    if (!collisions_is_segment_crossing_line(c, d, a, b)) {
+        return false;
+    }
+    return true;
+}
+
+bool collisions_is_line_crossing_circle(const pose_t *a, const pose_t *b,
+                                        const pose_t *center, const uint32_t radius)
+{
+    const pose_t *c = center;
+
+    pose_t vect_ab;
+
+    vect_ab.x = b->x - a->x;
+    vect_ab.y = b->y - a->y;
+
+    pose_t vect_ac;
+    vect_ac.x = c->x - a->x;
+    vect_ac.y = c->y - a->y;
+
+    /* Norm of vector V */
+    double numerator = vect_ab.x * vect_ac.y - vect_ab.y * vect_ac.x;
+    if (numerator < 0) {
+        numerator = -numerator;
+    }
+
+    /* Norm of vector U */
+    double denominator = sqrt(vect_ab.x * vect_ab.x + vect_ab.y * vect_ab.y);
+
+    /* Norm of vector CI where I is the nearest point of the line */
+    double ci = numerator / denominator;
+
+    /* If CI norm is less or equal to the circle radius, point I is inside the
+     * circle */
+    if (ci < radius) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool collisions_is_segment_crossing_circle(const pose_t *a, const pose_t *b,
+                                           const pose_t *center, const uint32_t radius)
+{
+    const pose_t *c = center;
+
+    if (!collisions_is_line_crossing_circle(a, b, center, radius)) {
+        return false;
+    }
+
+    if (collisions_is_point_in_circle(a, center, radius)) {
+        return true;
+    }
+    if (collisions_is_point_in_circle(b, center, radius)) {
+        return true;
+    }
+
+    pose_t vect_ab, vect_ac, vect_bc;
+    vect_ab.x = b->x - a->x;
+    vect_ab.y = b->y - a->y;
+    vect_ac.x = c->x - a->x;
+    vect_ac.y = c->y - a->y;
+    vect_bc.x = c->x - b->x;
+    vect_bc.y = c->y - b->y;
+
+    double scal1 = vect_ab.x * vect_ac.x + vect_ab.y * vect_ac.y;
+    double scal2 = (-vect_ab.x) * vect_bc.x + (-vect_ab.y) * vect_bc.y;
+    if (scal1 >= 0 && scal2 >= 0) {
+        return true;
+    }
+
+    return false;
+}
+
+bool collisions_is_point_on_segment(const pose_t *a, const pose_t *b, const pose_t *c)
+{
+    bool res = false;
+
+    if ((b->x - a->x) / (b->y - a->y) == (b->x - c->x) / (b->y - c->y)) {
+        if (a->x < b->x) {
+            if ((c->x < b->x) && (c->x > a->x)) {
+                res = true;
+            }
+        }
+        else {
+            if ((c->x < a->x) && (c->x > b->x)) {
+                res = true;
+            }
+        }
+    }
+    return res;
+}
+
+inline double collisions_compute_slope(const pose_t *a, const pose_t *b)
+{
+    return (b->y - a->y) / (b->x - a->x);
+}
+
+inline double collisions_compute_ordinate(double slope, const pose_t *b)
+{
+    return (b->y - slope * b->x);
+}
