ba3e7164a489a13555b940c1bbd1571d132030ce
Gilles DOFFE <g.doffe@gmail.com>
2021-05-15 23:55:39 +0200

    lib: avoidance: clean and comment code
    No algorithm changes. This commit adds doxygen, comments, rename
    functions and variables.
    It also changes return types to better suit needs.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- lib/avoidance/include/avoidance.h
+++ lib/avoidance/include/avoidance.h
@@ -1,3 +1,31 @@
+/*
+ * Copyright (C) 2021 COGIP Robotics association <cogip35@gmail.com>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    lib_avoidance Avoidance module
+ * @ingroup     lib
+ * @brief       Avoidance module
+ *
+ * Avoidance is split in 2 steps:
+ *   1. avoidance_build_graph(): build graph of avoidance path to reach finish
+ *      pose, including obstacles avoidance.
+ *   2. avoidance_get_pose(): get intermediate pose in avoidance computed path.
+ *
+ * Direct segment to reach final position can be checked to see if it becomes
+ * free of obstacles with avoidance_check_recompute()
+ *
+ * @{
+ * @file
+ * @brief       Public API for avoidance module
+ *
+ * @author      Gilles DOFFE <g.doffe@gmail.com>
+ */
+
 #pragma once
 
 /* Standard includes */
@@ -7,23 +35,53 @@
 /* Project includes */
 #include "cogip_defs.h"
 
-#define MAX_POINTS              256
-
-#define GRAPH_MAX_VERTICES      64
-#define DIJKSTRA_MAX_DISTANCE   13000000
-
-#define AVOIDANCE_GRAPH_ERROR   -1
-
-pose_t avoidance(uint8_t index);
-int update_graph(const pose_t *s, const pose_t *f);
-void build_avoidance_graph(void);
-bool is_colliding(const pose_t *start, const pose_t *stop);
+/**
+ * @name Dijkstra avoidance parameters
+ * @{
+ */
+#define GRAPH_MAX_VERTICES      64          /**< Maximum graph vertice number */
+#define DIJKSTRA_MAX_DISTANCE   UINT32_MAX  /**< Maximum Dijkstra distance */
+/** @} */
 
 /**
- * @brief Print list of intermediate position to reach wanted path position
+ * @brief Return given index pose in the avoidance path.
+ *
+ * @param[in]    index  pose index in the avoidance path
+ *
+ * @return              wanted pose if found, start position otherwise
+ */
+pose_t avoidance_get_pose(uint8_t index);
+
+/**
+ * @brief Build avoidance graph, listing all path possibilities between start
+ * and finish poses.
+ *
+ * @param[in]   start   start position
+ * @param[in]   finish  finish position
+ *
+ * @return              true if recompute is needed, false otherwise
+ */
+bool avoidance_build_graph(const pose_t *s, const pose_t *f);
+
+/**
+ * @brief Check if avoidance should be recomputed by checking if an obstacle is
+ * crossing the segment between start and finish position.
+ *
+ * @param[in]   start   start position
+ * @param[in]   finish  finish position
+ *
+ * @return              true if recompute is needed, false otherwise
+ */
+bool avoidance_check_recompute(const pose_t *start,
+                               const pose_t *stop);
+
+/**
+ * @brief Print list of intermediate positions to reach wanted path position
  *
  * @param[in]   out     file descriptor used to print avoidance path
  *
  * @return
  */
 void avoidance_print_path(FILE *out);
+
+/** @} */
