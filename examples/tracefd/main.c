c7ad6e62874aa4cf43ae7ffe38eca4dba2f8b154
Eric Courtois <eric.courtois@gmail.com>
2021-05-24 14:34:14 +0200

    sys: tracefd: add default stderr and stdout descriptors
    'tracefd_stderr' and 'tracefd_stdout' are default descriptors that
    can be used with 'tracefd_printf/lock/unlock' functions
    to lock stderr/stdout and safely write on them accross threads.
    
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- examples/tracefd/main.c
+++ examples/tracefd/main.c
@@ -12,32 +12,32 @@ int main(void)
 {
     tracefd_t tracefd;
 
-    puts("\n== Trace file descriptor example ==");
+    tracefd_printf(tracefd_stdout, "\n== Trace file descriptor example ==\n");
 
-    puts("Example 1: write in a file and close it");
-    tracefd = tracefd_init("example1.txt");
+    tracefd_printf(tracefd_stdout, "Example 1: write in a file and close it\n");
+    tracefd = tracefd_new("example1.txt");
     tracefd_open(tracefd);
     tracefd_printf(tracefd, "Trace example 1\n");
     tracefd_close(tracefd);
 
-    puts("Example 2: write in a file and do not close it");
-    tracefd = tracefd_init("example2.txt");
+    tracefd_printf(tracefd_stdout, "Example 2: write in a file and do not close it\n");
+    tracefd = tracefd_new("example2.txt");
     tracefd_open(tracefd);
     tracefd_printf(tracefd, "Trace example 2\n");
-    puts("  Wait 2s to be sure the files flusher thread has operated.");
+    tracefd_printf(tracefd_stdout, "  Wait 2s to be sure the files flusher thread has operated.\n");
     xtimer_sleep(2);
 
-    puts("Stop files flusher thread");
+    tracefd_printf(tracefd_stdout, "Stop files flusher thread\n");
     tracefd_stop_files_flusher();
 
-    puts("Example 3: write in a file and do not close it");
-    tracefd = tracefd_init("example3.txt");
+    tracefd_printf(tracefd_stdout, "Example 3: write in a file and do not close it\n");
+    tracefd = tracefd_new("example3.txt");
     tracefd_open(tracefd);
     tracefd_printf(tracefd, "Trace example 3\n");
 
-    puts("== End of example ==");
+    tracefd_printf(tracefd_stdout, "== End of example ==\n");
 
-    puts("On real hardware, without automatic flushing, 'example3.txt' should be empty.\n");
+    tracefd_printf(tracefd_stdout, "On real hardware, without automatic flushing, 'example3.txt' should be empty.\n\n");
 
     return 0;
 }
