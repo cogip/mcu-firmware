b6ff77dffa889257890a597e6060e494f8553a6f
Eric Courtois <eric.courtois@gmail.com>
2021-09-03 16:32:04 +0200

    lib: obstacles: move class Circle to its own file
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

---
+++ lib/obstacles/Circle.cpp
@@ -0,0 +1,110 @@
+#include "obstacles/Circle.hpp"
+
+// System includes
+#include <cmath>
+
+namespace cogip {
+
+namespace obstacles {
+
+Circle::Circle(const cogip_defs::Coords &center, double radius, double angle)
+    : Obstacle(center, radius, angle)
+{
+}
+
+bool Circle::is_point_inside(const cogip_defs::Coords &p) const {
+    double d = center_.distance(p);
+
+    if (d * d > radius_ * radius_) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+bool Circle::is_segment_crossing(const cogip_defs::Coords &a, const cogip_defs::Coords &b) const
+{
+    const cogip_defs::Coords &c = center_;
+
+    if (!is_line_crossing_circle(a, b)) {
+        return false;
+    }
+
+    if (is_point_inside(a)) {
+        return true;
+    }
+    if (is_point_inside(b)) {
+        return true;
+    }
+
+    cogip_defs::Coords vect_ab(b.x() - a.x(), b.y() - a.y());
+    cogip_defs::Coords vect_ac(c.x() - a.x(), c.y() - a.y());
+    cogip_defs::Coords vect_bc(c.x() - b.x(), c.y() - b.y());
+
+    double scal1 = vect_ab.x() * vect_ac.x() + vect_ab.y() * vect_ac.y();
+    double scal2 = (-vect_ab.x()) * vect_bc.x() + (-vect_ab.y()) * vect_bc.y();
+    if (scal1 >= 0 && scal2 >= 0) {
+        return true;
+    }
+
+    return false;
+}
+
+cogip_defs::Coords Circle::nearest_point(const cogip_defs::Coords &p) const
+{
+    cogip_defs::Coords vect(
+        p.x() - center_.x(),
+        p.y() - center_.y()
+    );
+
+    double vect_norm = sqrt(vect.x() * vect.x() + vect.y() * vect.y());
+
+    return cogip_defs::Coords(
+        center_.x() + (vect.x() / vect_norm) * radius_,
+        center_.y() + (vect.y() / vect_norm) * radius_
+    );
+}
+
+void Circle::print_json(cogip::tracefd::File &out) const
+{
+    out.printf(
+        "{\"x\":%.3lf,\"y\":%.3lf,\"radius\":%.3lf}",
+        center_.x(),
+        center_.y(),
+        radius_
+        );
+}
+
+bool Circle::is_line_crossing_circle(const cogip_defs::Coords &a, const cogip_defs::Coords &b) const
+{
+    const cogip_defs::Coords &c = center_;
+
+    cogip_defs::Coords vect_ab(b.x() - a.x(), b.y() - a.y());
+    cogip_defs::Coords vect_ac(c.x() - a.x(), c.y() - a.y());
+
+    // Norm of vector V
+    double numerator = vect_ab.x() * vect_ac.y() - vect_ab.y() * vect_ac.x();
+    if (numerator < 0) {
+        numerator = -numerator;
+    }
+
+    // Norm of vector U
+    double denominator = sqrt(vect_ab.x() * vect_ab.x() + vect_ab.y() * vect_ab.y());
+
+    // Norm of vector CI where I is the nearest point of the line
+    double ci = numerator / denominator;
+
+    // If CI norm is less or equal to the circle radius, point I is inside the
+    // circle
+    if (ci < radius_) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+} // namespace obstacles
+
+} // namespace cogip
---
+++ lib/obstacles/include/obstacles/Circle.hpp
@@ -0,0 +1,48 @@
+// Copyright (C) 2021 COGIP Robotics association <cogip35@gmail.com>
+// This file is subject to the terms and conditions of the GNU Lesser
+// General Public License v2.1. See the file LICENSE in the top level
+// directory for more details.
+
+/// @ingroup     lib_obstacles
+/// @{
+/// @file
+/// @brief       Circle obstacle class declaration
+/// @author      Eric Courtois <eric.courtois@gmail.com>
+
+#pragma once
+
+#include "obstacles/obstacles.hpp"
+
+namespace cogip {
+
+namespace obstacles {
+
+/// Circle obstacle defined by its center and radius
+class Circle : public Obstacle {
+public:
+    /// Constructor
+    Circle(
+        const cogip::cogip_defs::Coords &center, ///< [in] center of the circle
+        double radius,                           ///< [in] radius of the circle
+        double angle                             ///< [in] angle of the obstacle
+        );
+
+    bool is_point_inside(const cogip_defs::Coords &p) const override;
+    bool is_segment_crossing(const cogip_defs::Coords &a, const cogip_defs::Coords &b) const override;
+    cogip_defs::Coords nearest_point(const cogip_defs::Coords &p) const override;
+    void print_json(cogip::tracefd::File &out) const override;
+
+private:
+    /// Check if a line defined by two points A,B is crossing a circle.
+    /// @return true if (AB) crosses circle, false otherwise
+    bool is_line_crossing_circle(
+        const cogip_defs::Coords &a, ///< point A
+        const cogip_defs::Coords &b  ///< point B
+        ) const;
+};
+
+} // namespace obstacles
+
+} // namespace cogip
+
+/// @}
