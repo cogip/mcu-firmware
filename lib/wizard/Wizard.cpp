5507c8c7edca17813ebd0766874edae76115760a
Eric Courtois <eric.courtois@gmail.com>
2022-04-17 18:08:46 +0200

    lib: wizard: add new library
    This module is used to communicate with the Copilot at the beginning
    of a game to configure dynamic parameters or play a check-list.
    It provides predefined Protobuf messages (bool, int, float,
    str, list, ...) that can be sent using uartpb module.
    After sending a message, the module waits for a response of the same type, with
    its value updated.
    
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

---
+++ lib/wizard/Wizard.cpp
@@ -0,0 +1,39 @@
+// Project includes
+#include "wizard/Wizard.hpp"
+#include "uartpb_config.hpp"
+
+namespace cogip {
+
+namespace wizard {
+
+static PB_OutputMessage output_message;
+
+Wizard::Wizard(cogip::uartpb::UartProtobuf *uartpb) : uartpb_(uartpb)
+{
+    event_queue_init(&queue_);
+}
+
+void Wizard::handle_response(const PB_Message &pb_message)
+{
+    puts("handle_response");
+    event_.pb_message = pb_message;
+    event_post(&queue_, (event_t *)&event_);
+}
+
+const Wizard::PB_Message &Wizard::request(const PB_Message &request)
+{
+    if (! uartpb_) {
+        return request;
+    }
+    output_message.set_wizard(request);
+    uartpb_->send_message(output_message);
+
+    wizard_event_t *event = (wizard_event_t *)event_wait(&queue_);
+    return event->pb_message;
+}
+
+} // namespace wizard
+
+} // namespace cogip
+
+/// @}
