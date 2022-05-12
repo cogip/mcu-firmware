652b9b944e23442709278fecc43d631de13bc32a
Gilles DOFFE <g.doffe@gmail.com>
2022-05-12 20:49:53 +0200

    applications: introduce cup2022 application
    This is the 2022 Eurobot Robotic contest application.
    For now it is a copy of app_test appliction with configuration values
    updated to suit to the new robot and contest.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

---
+++ applications/cup2022/app_path.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+
+#include "path/Path.hpp"
+#include "app.hpp"
+#include "platform.hpp"
+
+// TODO:
+// - Add anti-blocking activation for each point,
+// - For each actions: keep a status if its done (to avoid multiple puck capture
+//    if delivery was bypassed for any reason)
+// - Absolute coordinate recalibration on games frames (?)
+static std::vector<cogip::path::Pose> poses = {
+    {
+        -1200, 700, 0,
+        MAX_SPEED
+    },
+    {
+        0, 700, 180,
+        LOW_SPEED
+    },
+    {
+        1200, 700, 0,
+        NORMAL_SPEED
+    },
+};
+
+cogip::path::Path *_path = nullptr;
+
+cogip::path::Path &app_get_path(void)
+{
+    if (! _path) {
+        _path = new cogip::path::Path(poses, true);
+    }
+    return *_path;
+}
---
+++ applications/cup2022/include/app.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+// Project includes
+#include "obstacles/Polygon.hpp"
+#include "path/Path.hpp"
+#include "uartpb/ReadBuffer.hpp"
+
+/*
+ * Machine parameters
+ */
+
+#define USART_CONSOLE   USARTC0
+
+/// Return a polygon obstacle delimitng the table borders.
+const cogip::obstacles::Polygon &app_get_borders(void);
+
+/// Return the path of current application.
+cogip::path::Path &app_get_path(void);
+
+/// Message handler for incoming Protobuf messages.
+void app_message_handler(cogip::uartpb::ReadBuffer &buffer);
+
+void app_init(void);
+void app_init_tasks(void);
+
+/// Run Application wizard
+void app_wizard(void);
