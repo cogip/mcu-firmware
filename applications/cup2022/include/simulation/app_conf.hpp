7db6383025cc1ab5eef00a63cbd07e6b052f68b4
Gilles DOFFE <g.doffe@gmail.com>
2022-05-21 00:01:01 +0200

    applications: tune PIDs for simulation
    Use same gains for all applications as for now they use the same
    maximum speed and acceleration.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- applications/cup2022/include/simulation/app_conf.hpp
+++ applications/cup2022/include/simulation/app_conf.hpp
@@ -5,28 +5,28 @@
 
 static const ctrl_quadpid_parameters_t ctrl_quadpid_params = {
     .linear_speed_pid = {
-        .kp = 10,
-        .ki = 0.05,
+        .kp = 25,
+        .ki = 1,
         .kd = 0.,
         .ti = 0.,
         .previous_error = 0.
     },
     .angular_speed_pid = {
-        .kp = 10,
-        .ki = 0.05,
+        .kp = 25,
+        .ki = 1,
         .kd = 0.,
         .ti = 0.,
         .previous_error = 0.
     },
     .linear_pose_pid = {
-        .kp = 1,
+        .kp = 0.1,
         .ki = 0.,
         .kd = 0,
         .ti = 0.,
         .previous_error = 0.
     },
     .angular_pose_pid = {
-        .kp = 1,
+        .kp = 0.1,
         .ki = 0.,
         .kd = 0,
         .ti = 0.,
