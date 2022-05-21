3b47eca774a68ae1b6cb0c074a1507fb0ea36190
Eric Courtois <eric.courtois@gmail.com>
2022-05-21 18:54:09 +0200

    applications: app_test: run wizard shell command in a thread
    wizard command uses uartpb and can be triggered by uartpb message,
    which leads to a deadlock.
    So run the command in a different thread to avoid this lock.
    
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- applications/app_test/app_shell.cpp
+++ applications/app_test/app_shell.cpp
@@ -4,15 +4,36 @@
 #include "platform.hpp"
 #include "shell_menu/shell_menu.hpp"
 
+#include "thread.h"
+
+static char wizard_thread_stack[THREAD_STACKSIZE_MEDIUM];
+#define WIZARD_THREAD_PRIO (THREAD_PRIORITY_MAIN - 1)
+
 namespace cogip {
 
 namespace app {
+
+static void *_wizard_thread_command(void *arg)
+{
+    (void)arg;
+    app_wizard();
+    return EXIT_SUCCESS;
+}
 
 static int _cmd_wizard(int argc, char **argv)
 {
     (void)argc;
     (void)argv;
-    app_wizard();
+    thread_create(
+        wizard_thread_stack,
+        sizeof(wizard_thread_stack),
+        WIZARD_THREAD_PRIO,
+        0,
+        _wizard_thread_command,
+        NULL,
+        "Wizard"
+        );
+
     return 0;
 }
 
@@ -20,14 +41,14 @@ static int _cmd_samples(int argc, char **argv)
 {
     (void)argc;
     (void)argv;
-    cogip::app::app_samples_request();
+    cogip::app::app_samples_detect();
     return 0;
 }
 
 void app_shell_init()
 {
     cogip::shell::root_menu.push_back(
-        new cogip::shell::Command("_wizard", "Run Wizard", _cmd_wizard));
+        new cogip::shell::Command("wizard", "Run Wizard", _cmd_wizard));
 
     cogip::shell::root_menu.push_back(
         new cogip::shell::Command("_samples", "Get detected samples", _cmd_samples));
