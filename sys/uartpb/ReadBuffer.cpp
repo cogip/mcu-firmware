3cd5a330ea3af5f04b7deec72b5387d9b2e19b6a
Eric Courtois <eric.courtois@gmail.com>
2022-07-10 20:08:02 +0200

    sys: uartpb: synchronize ReadBuffer API with new EmbeddedProto interface
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- sys/uartpb/ReadBuffer.cpp
+++ sys/uartpb/ReadBuffer.cpp
@@ -31,21 +31,29 @@ uint32_t ReadBuffer::get_max_size() const
 bool ReadBuffer::peek(uint8_t& byte) const
 {
   bool return_value = write_index_ > read_index_;
-  if (return_value)
-  {
+  if (return_value) {
     byte = data_[read_index_];
   }
   return return_value;
 }
 
-void ReadBuffer::advance()
+bool ReadBuffer::advance()
 {
-  ++read_index_;
+  const bool return_value = write_index_ > read_index_;
+  if(return_value) {
+    ++read_index_;
+  }
+  return return_value;
 }
 
-void ReadBuffer::advance(const uint32_t n)
+bool ReadBuffer::advance(const uint32_t n)
 {
-  read_index_ += n;
+  const uint32_t new_read_index = read_index_ + n;
+  const bool return_value = write_index_ > new_read_index;
+  if(return_value) {
+    read_index_ = new_read_index;
+  }
+  return return_value;
 }
 
 bool ReadBuffer::pop(uint8_t &byte)
@@ -78,8 +86,7 @@ void ReadBuffer::clear()
 bool ReadBuffer::push(uint8_t &byte)
 {
   bool return_value = UARTPB_INPUT_MESSAGE_LENGTH_MAX > write_index_;
-  if (return_value)
-  {
+  if (return_value) {
     data_[write_index_] = byte;
     ++write_index_;
   }
