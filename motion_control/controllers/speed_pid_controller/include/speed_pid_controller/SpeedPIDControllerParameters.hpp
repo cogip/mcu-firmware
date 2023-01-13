ae00abec503a0e3ce2ab617e764aafd950fd1f8b
Gilles DOFFE <g.doffe@gmail.com>
2023-01-13 02:25:53 +0100

    motion_control: introduce speed_pid_controller
    Compute a PID correction command according to speed error as input.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

---
+++ motion_control/controllers/speed_pid_controller/include/speed_pid_controller/SpeedPIDControllerParameters.hpp
@@ -0,0 +1,43 @@
+// Copyright (C) 2023 COGIP Robotics association <cogip35@gmail.com>
+// This file is subject to the terms and conditions of the GNU Lesser
+// General Public License v2.1. See the file LICENSE in the top level
+// directory for more details.
+
+/// @ingroup    speed_pid_controller Speed PID controller parameters
+/// @{
+/// @file
+/// @brief      Speed PID controller parameters
+/// @author     Eric Courtois <eric.courtois@gmail.com>
+/// @author     Gilles DOFFE <g.doffe@gmail.com>
+
+#pragma once
+
+// Project includes
+#include "pid_ng/PID.hpp"
+
+namespace cogip {
+
+namespace motion_control {
+
+/// Speed PID controller parameters
+class SpeedPIDControllerParameters {
+public:
+    /// Constructor
+    SpeedPIDControllerParameters(
+        pid::PID *pid = nullptr     ///< [in]  PID parameters
+    ) : pid_(pid) {};
+
+    /// Get PID parameters
+    /// return     PID parameters pointer
+    pid::PID *pid() const { return pid_; };
+
+private:
+    /// PID parameters
+    pid::PID *pid_;   ///< Speed PID
+};
+
+} // namespace motion_control
+
+} // namespace cogip
+
+/// @}
