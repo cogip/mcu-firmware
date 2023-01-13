436585d4347270f2552113fa7500551ed21fd5cc
Gilles DOFFE <g.doffe@gmail.com>
2023-01-13 02:28:28 +0100

    motion_control: introduce pose_pid_controller
    Compute a speed order according to pose error as input.
    Current speed and target speed are passed through this controller to
    speed controller.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

---
+++ motion_control/controllers/pose_pid_controller/include/pose_pid_controller/PosePIDControllerParameters.hpp
@@ -0,0 +1,43 @@
+// Copyright (C) 2023 COGIP Robotics association <cogip35@gmail.com>
+// This file is subject to the terms and conditions of the GNU Lesser
+// General Public License v2.1. See the file LICENSE in the top level
+// directory for more details.
+
+/// @ingroup    pose_pid_controller Pose PID controller parameters
+/// @{
+/// @file
+/// @brief      Pose PID controller parameters
+/// @author     Eric Courtois <eric.courtois@gmail.com>
+/// @author     Gilles DOFFE <g.doffe@gmail.com>
+
+#pragma once
+
+// Project includes
+#include "pid_ng/PID.hpp"
+
+namespace cogip {
+
+namespace motion_control {
+
+/// Pose controller parameters
+class PosePIDControllerParameters {
+public:
+    /// Constructor
+    PosePIDControllerParameters(
+        pid::PID *pid = nullptr     ///< [in]  PID parameters
+    ) : pid_(pid) {};
+
+    /// Get PID parameters
+    /// return     PID parameters pointer
+    pid::PID *pid() const { return pid_; };
+
+private:
+    /// PID parameters
+    pid::PID *pid_;   ///< Position PID
+};
+
+} // namespace motion_control
+
+} // namespace cogip
+
+/// @}
