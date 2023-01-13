b2e0f9006fc3accc2f46011ee4989240e279efb4
Gilles DOFFE <g.doffe@gmail.com>
2023-01-13 02:32:41 +0100

    motion_control: filters: introduce pose_straight_filter
    Breaks down a movement into a straight trajectory.
    The robot first orients itself towards the point to be reached, then
    goes towards this point in a straight line.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

---
+++ motion_control/filters/pose_straight_filter/PoseStraightFilter.cpp
@@ -0,0 +1,114 @@
+// RIOT includes
+#include <ztimer.h>
+
+// System includes
+#include <cstdio>
+#include <iostream>
+
+// Project includes
+#include "cogip_defs/Pose.hpp"
+#include "cogip_defs/Polar.hpp"
+#include "etl/list.h"
+#include "etl/vector.h"
+
+#include "pose_straight_filter/PoseStraightFilter.hpp"
+
+namespace cogip {
+
+namespace motion_control {
+
+void PoseStraightFilter::execute() {
+    std::cout << "Execute PoseStraightFilter" << std::endl;
+
+    size_t input_index = 0;
+
+    // Current pose
+    double current_pose_x = this->inputs_[input_index++];
+    double current_pose_y = this->inputs_[input_index++];
+    double current_pose_O = this->inputs_[input_index++];
+    cogip_defs::Pose current_pose(
+        current_pose_x,
+        current_pose_y,
+        current_pose_O
+    );
+
+    // Target pose
+    double target_pose_x = this->inputs_[input_index++];
+    double target_pose_y = this->inputs_[input_index++];
+    double target_pose_O = this->inputs_[input_index++];
+    cogip_defs::Pose target_pose(
+        target_pose_x,
+        target_pose_y,
+        target_pose_O
+    );
+
+    // Current speed
+    double current_linear_speed = this->inputs_[input_index++];
+    double current_angular_speed = this->inputs_[input_index++];
+    cogip_defs::Polar current_speed(
+        current_linear_speed,
+        current_angular_speed
+    );
+
+    // Target speed
+    double target_linear_speed = this->inputs_[input_index++];
+    double target_angular_speed = this->inputs_[input_index++];
+    cogip_defs::Polar target_speed(
+        target_linear_speed,
+        target_angular_speed
+    );
+
+    if (!is_index_valid(input_index)) {
+        return;
+    }
+
+    // Allow moving reversely
+    bool allow_reverse = this->inputs_[10];
+
+    // compute position error
+    cogip_defs::Polar pos_err = target_pose - current_pose;
+
+    // position correction */
+    if (fabs(pos_err.distance()) > parameters_->linear_treshold()) {
+        // go reverse ? */
+        if (allow_reverse && fabs(pos_err.angle()) > 90) {
+            pos_err.reverse();
+        }
+
+        // if target point direction angle is too important, bot rotates on its starting point
+        if (fabs(pos_err.angle()) > parameters_->angular_treshold()) {
+            pos_err.set_distance(0);
+        }
+    }
+    else {
+        // orientation correction only (position is reached)
+        pos_err.set_distance(0);
+
+        // final orientation error
+        // TODO: should not be necessary, already done in 'pos_err = target_pose - current_pose'
+        //pos_err.set_angle(limit_angle_deg(target_pose.O() - current_pose.O()));
+
+        // orientation is reached
+        if (fabs(pos_err.angle()) < parameters_->angular_treshold()) {
+            pos_err.set_angle(0);
+        }
+    }
+
+    // Linear pose error
+    this->outputs_[0] = pos_err.distance();
+    // Linear current speed
+    this->outputs_[1] = current_speed.distance();
+    // Linear target speed
+    this->outputs_[2] = target_speed.distance();
+
+    // Angular pose error
+    this->outputs_[3] = pos_err.angle();
+    // Angular current speed
+    this->outputs_[4] = current_speed.angle();
+    // Angular target speed
+    this->outputs_[5] = target_speed.angle();
+};
+
+} // namespace motion_control
+
+} // namespace cogip
---
+++ motion_control/filters/pose_straight_filter/include/pose_straight_filter/PoseStraightFilter.hpp
@@ -0,0 +1,50 @@
+// Copyright (C) 2023 COGIP Robotics association <cogip35@gmail.com>
+// This file is subject to the terms and conditions of the GNU Lesser
+// General Public License v2.1. See the file LICENSE in the top level
+// directory for more details.
+
+/// @ingroup    pose_straight_filter Pose straight filter
+/// @{
+/// @file
+/// @brief      Breaks down a movement into a straight trajectory
+/// @author     Eric Courtois <eric.courtois@gmail.com>
+/// @author     Gilles DOFFE <g.doffe@gmail.com>
+
+#pragma once
+
+// Project includes
+#include "motion_control_common/Controller.hpp"
+#include "PoseStraightFilterParameters.hpp"
+
+namespace cogip {
+
+namespace motion_control {
+
+/// Breaks down a movement into a straight trajectory.
+///        The robot first orients itself towards the point to be reached, then goes towards this point in a straight line.
+/// Input 0-2:  current pose
+/// Input 3-5:  target pose
+/// Input 6-7:  current speed
+/// Input 8-9:  target speed
+/// Output 0:   linear pose error
+/// Output 1:   linear current speed
+/// Output 2:   linear target speed
+/// Output 3:   angular pose error
+/// Output 4:   angular current speed
+/// Output 5:   angular target speed
+class PoseStraightFilter : public Controller<10, 6, PoseStraightFilterParameters> {
+public:
+    /// Constructor
+    explicit PoseStraightFilter(
+        PoseStraightFilterParameters *parameters    ///< [in]  Movements switch tresholds. See PoseStraightFilterParameters.
+        ) : Controller(parameters) {};
+
+    /// Breaks down a movement into a straight trajectory according to movements switch tresholds.
+    void execute() override;
+};
+
+} // namespace motion_control
+
+} // namespace cogip
+
+/// @}
