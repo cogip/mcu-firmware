2b2bd9be8db3488861fbe1785104c5139d0acb6d
Gilles DOFFE <g.doffe@gmail.com>
2023-02-09 14:58:19 +0100

    lib: utils: use DEBUG flag to print or not debug traces
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- lib/utils/include/utils.hpp
+++ lib/utils/include/utils.hpp
@@ -14,5 +14,11 @@ typedef void (*func_cb_t)(void);
 #define MIN(a, b)   (((a) < (b)) ? (a) : (b))
 #define MAX(a, b)   (((a) > (b)) ? (a) : (b))
 
-#define COGIP_DEBUG_COUT(x)    (std::cout << x << std::endl)
-#define COGIP_DEBUG_CERR(x)    (std::cout << x << std::endl)
+#ifdef DEBUG
+    #define COGIP_DEBUG_COUT(x)    (std::cout << x << std::endl)
+    #define COGIP_DEBUG_CERR(x)    (std::cout << x << std::endl)
+    #undef DEBUG
+#else
+    #define COGIP_DEBUG_COUT(x)
+    #define COGIP_DEBUG_CERR(x)
+#endif
