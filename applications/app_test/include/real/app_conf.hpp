006b8206c24051d61aa93cd3d7332d8f06d5fe72
Gilles DOFFE <g.doffe@gmail.com>
2023-04-12 18:51:16 +0200

    applications: cup2023/app_test: setup real PIDs and thresholds
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- applications/app_test/include/real/app_conf.hpp
+++ applications/app_test/include/real/app_conf.hpp
@@ -4,30 +4,30 @@
 #include "etl/numeric.h"
 
 // Linear pose PID
-constexpr double linear_pose_pid_kp = 0.06;
+constexpr double linear_pose_pid_kp = 0.130;
 constexpr double linear_pose_pid_ki = 0;
 constexpr double linear_pose_pid_kd = 0;
 // Angular pose PID
-constexpr double angular_pose_pid_kp = 0.14;
+constexpr double angular_pose_pid_kp = 0.05;
 constexpr double angular_pose_pid_ki = 0;
 constexpr double angular_pose_pid_kd = 0;
 // Linear speed PID
-constexpr double linear_speed_pid_kp = 50;
+constexpr double linear_speed_pid_kp = 70;
 constexpr double linear_speed_pid_ki = 1;
 constexpr double linear_speed_pid_kd = 0;
 // Angular speed PID
-constexpr double angular_speed_pid_kp = 120;
+constexpr double angular_speed_pid_kp = 250;
 constexpr double angular_speed_pid_ki = 1.5;
 constexpr double angular_speed_pid_kd = 0;
 
 // Linear pose PID integral limit
-constexpr double linear_pose_pid_integral_limit = etl::numeric_limits<uint16_t>::max();
+constexpr double linear_pose_pid_integral_limit = etl::numeric_limits<int16_t>::max();
 // Angular pose PID integral limit
-constexpr double angular_pose_pid_integral_limit = etl::numeric_limits<uint16_t>::max();
+constexpr double angular_pose_pid_integral_limit = etl::numeric_limits<int16_t>::max();
 // Linear speed PID integral limit
-constexpr double linear_speed_pid_integral_limit = etl::numeric_limits<uint16_t>::max();
+constexpr double linear_speed_pid_integral_limit = etl::numeric_limits<int16_t>::max() / 4;
 // Angular speed PID integral limit
-constexpr double angular_speed_pid_integral_limit = etl::numeric_limits<uint16_t>::max();
+constexpr double angular_speed_pid_integral_limit = etl::numeric_limits<int16_t>::max() / 4;
 
 // Linear treshold
 constexpr double linear_treshold = 5;
