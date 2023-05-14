8af1bec44534c41f1f74b00030b294859b85550a
Gilles DOFFE <g.doffe@gmail.com>
2023-05-14 20:33:41 +0200

    platforms: pegasus: introduce AnalogServo
    AnalogServo drives analog servomotors. Each AnalogServo object registers
    several pre-defined positions to reach. The command is the ID of the
    wanted position.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

---
+++ platforms/pegasus/AnalogServo.cpp
@@ -0,0 +1,57 @@
+// Copyright (C) 2023 COGIP Robotics association <cogip35@gmail.com>
+// This file is subject to the terms and conditions of the GNU Lesser
+// General Public License v2.1. See the file LICENSE in the top level
+// directory for more details.
+
+#include "AnalogServo.hpp"
+#include "pf_positional_actuators.hpp"
+
+#include <periph/gpio.h>
+#include <pcf857x.h>
+
+#include <iostream>
+
+namespace cogip {
+namespace pf {
+namespace actuators {
+namespace positional_actuators {
+
+pca9685_t AnalogServo::pca9685_dev;
+
+AnalogServo::AnalogServo(
+    Enum id,
+    GroupEnum group,
+    uint8_t order,
+    int channel
+) : PositionalActuator(id, group, order), channel_(channel) {
+}
+
+void AnalogServo::add_position(uint16_t position) {
+    if (!positions_.full()) {
+        positions_.push_back(position);
+    }
+    else {
+        std::cerr << "ERROR: Servomotor " << channel_ << " positions list is full!" << std::endl;
+    }
+}
+
+void AnalogServo::disable() {
+    pca9685_pwm_set(&AnalogServo::pca9685_dev, channel_, 0);
+}
+
+void AnalogServo::actuate(int32_t command) {
+    if ((command >= 0) && (static_cast<unsigned int>(command) < positions_.size())) {
+        pca9685_pwm_set(&AnalogServo::pca9685_dev, channel_, positions_.at(command));
+        std::cout << "INFO: Servomotor " << channel_ << " at position " << positions_.at(command) << std::endl;
+    }
+    else {
+        std::cerr << "ERROR: Servomotor " << channel_ << " command out of range!" << std::endl;
+    }
+
+    command_ = command;
+}
+
+} // namespace positional_actuators
+} // namespace actuators
+} // namespace pf
+} // namespace cogip
---
+++ platforms/pegasus/include/AnalogServo.hpp
@@ -0,0 +1,66 @@
+// Copyright (C) 2023 COGIP Robotics association <cogip35@gmail.com>
+// This file is subject to the terms and conditions of the GNU Lesser
+// General Public License v2.1. See the file LICENSE in the top level
+// directory for more details.
+
+/// @ingroup     platforms_pegasus
+/// @{
+/// @file
+/// @brief       C++ class representing a on/off actuator using GPIOs.
+/// @author      Gilles DOFFE <g.doffe@gmail.com>
+
+#pragma once
+
+#include "PositionalActuator.hpp"
+
+#include <etl/vector.h>
+#include <pca9685.h>
+
+#include <iosfwd>
+
+namespace cogip {
+namespace pf {
+namespace actuators {
+namespace positional_actuators {
+
+constexpr uint8_t max_positions = 10;
+
+/// Class representing a servomotor driven by pca9685.
+class AnalogServo: public PositionalActuator {
+public:
+    /// Constructor.
+    AnalogServo(
+        Enum id,                            ///< [in] motor id
+        GroupEnum group,                    ///< [in] actuator group
+        uint8_t order = 0,                  ///< [in] order in actuator group
+        int servo_id = 0                    ///< [in] Servomotor ID on PCA9685
+    );
+
+    /// Add new position
+    void add_position(uint16_t);
+
+    /// Disable the motor.
+    void disable();
+
+    /// Disable the positional actuator after conditions.
+    bool disable_on_check() { disable(); return true; };
+
+    /// Activate the motor.
+    void actuate(
+        const int32_t command               ///< [in] servomotor position
+    );
+
+    static pca9685_t pca9685_dev;           ///< PCA9685 I2C PWM driver
+
+private:
+    int channel_ = 0;                       ///< Servomotor ID on PCA9685
+
+    etl::vector<uint16_t, max_positions> positions_;    ///< List of servomotor positions
+};
+
+} // namespace motors
+} // namespace actuators
+} // namespace app
+} // namespace cogip
+
+/// @}
