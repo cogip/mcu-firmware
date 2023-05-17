d27a621e678636e8fa78c04ca8704f70fb023da4
Gilles DOFFE <g.doffe@gmail.com>
2023-05-17 09:31:18 +0200

    applications: cup2023: tune 2023 cup parameters
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- applications/cup2023/include/real/app_conf.hpp
+++ applications/cup2023/include/real/app_conf.hpp
@@ -4,20 +4,20 @@
 #include "etl/numeric.h"
 
 // Linear pose PID
-constexpr double linear_pose_pid_kp = 0.130;
+constexpr double linear_pose_pid_kp = 0.035;
 constexpr double linear_pose_pid_ki = 0;
 constexpr double linear_pose_pid_kd = 0;
 // Angular pose PID
-constexpr double angular_pose_pid_kp = 0.05;
+constexpr double angular_pose_pid_kp = 0.07;
 constexpr double angular_pose_pid_ki = 0;
 constexpr double angular_pose_pid_kd = 0;
 // Linear speed PID
 constexpr double linear_speed_pid_kp = 70;
-constexpr double linear_speed_pid_ki = 1;
+constexpr double linear_speed_pid_ki = 2;
 constexpr double linear_speed_pid_kd = 0;
 // Angular speed PID
-constexpr double angular_speed_pid_kp = 250;
-constexpr double angular_speed_pid_ki = 1.5;
+constexpr double angular_speed_pid_kp = 300;
+constexpr double angular_speed_pid_ki = 1;
 constexpr double angular_speed_pid_kd = 0;
 
 // Linear pose PID integral limit
@@ -30,9 +30,9 @@ constexpr double linear_speed_pid_integral_limit = etl::numeric_limits<int16_t>:
 constexpr double angular_speed_pid_integral_limit = etl::numeric_limits<int16_t>::max() / 4;
 
 // Linear treshold
-constexpr double linear_treshold = 5;
+constexpr double linear_treshold = 10;
 // Angular treshold
-constexpr double angular_treshold = 5;
+constexpr double angular_treshold = 4;
 // Linear deceleration treshold
-constexpr double linear_deceleration_treshold = 100;
+constexpr double linear_deceleration_treshold = 400;
 
