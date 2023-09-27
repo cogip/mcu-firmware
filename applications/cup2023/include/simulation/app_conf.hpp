a7101d253c93f57090ee96ddee50a5bedd28f048
Gilles DOFFE <g.doffe@gmail.com>
2023-09-27 23:51:46 +0200

    applications: cup2023: set speed PIDs gains to 0
    As QDEC values are simulated and set to theorical values, it overrides
    speed PIDs control.
    Thus set them to 0.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- applications/cup2023/include/simulation/app_conf.hpp
+++ applications/cup2023/include/simulation/app_conf.hpp
@@ -12,12 +12,12 @@ constexpr double angular_pose_pid_kp = 0.065;
 constexpr double angular_pose_pid_ki = 0;
 constexpr double angular_pose_pid_kd = 0;
 // Linear speed PID
-constexpr double linear_speed_pid_kp = 125;
-constexpr double linear_speed_pid_ki = 40;
+constexpr double linear_speed_pid_kp = 0;
+constexpr double linear_speed_pid_ki = 0;
 constexpr double linear_speed_pid_kd = 0;
 // Angular speed PID
-constexpr double angular_speed_pid_kp = 250;
-constexpr double angular_speed_pid_ki = 75;
+constexpr double angular_speed_pid_kp = 0;
+constexpr double angular_speed_pid_ki = 0;
 constexpr double angular_speed_pid_kd = 0;
 
 // Linear pose PID integral limit
