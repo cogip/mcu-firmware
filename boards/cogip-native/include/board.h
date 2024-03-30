e63afed808f753ef7541d488ea2a684e293876cf
Gilles DOFFE <g.doffe@gmail.com>
2024-03-30 17:09:38 +0100

    fix UART

--- boards/cogip-native/include/board.h
+++ boards/cogip-native/include/board.h
@@ -48,7 +48,7 @@ enum {
 
 /* Servomotors */
 #define LX_DIR_PIN      GPIO_PIN(PORT_B, 3)
-#define LX_UART_DEV     1
+#define LX_UART_DEV     2
 
 /* Motion motors simulation */
 #define MOTION_MOTORS_POST_CB cogip_native_motor_driver_qdec_simulation
