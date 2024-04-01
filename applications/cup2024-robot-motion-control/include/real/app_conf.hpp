e5fbc88e0e37f9fb83ec2bf57fc1fa9d925b112c
Gilles DOFFE <g.doffe@gmail.com>
2024-04-01 15:49:57 +0200

    2024 tuning app_conf.hpp

--- applications/cup2024-robot-motion-control/include/real/app_conf.hpp
+++ applications/cup2024-robot-motion-control/include/real/app_conf.hpp
@@ -8,7 +8,7 @@ constexpr double linear_pose_pid_kp = 0.1;
 constexpr double linear_pose_pid_ki = 0;
 constexpr double linear_pose_pid_kd = 0;
 // Angular pose PID
-constexpr double angular_pose_pid_kp = 0.2;
+constexpr double angular_pose_pid_kp = 0.4;
 constexpr double angular_pose_pid_ki = 0;
 constexpr double angular_pose_pid_kd = 0;
 // Linear speed PID
@@ -17,7 +17,7 @@ constexpr double linear_speed_pid_ki = 1.5;
 constexpr double linear_speed_pid_kd = 0;
 // Angular speed PID
 constexpr double angular_speed_pid_kp = 50;
-constexpr double angular_speed_pid_ki = 3;
+constexpr double angular_speed_pid_ki = 5;
 constexpr double angular_speed_pid_kd = 0;
 
 // Linear pose PID integral limit
@@ -30,7 +30,7 @@ constexpr double linear_speed_pid_integral_limit = etl::numeric_limits<int16_t>:
 constexpr double angular_speed_pid_integral_limit = etl::numeric_limits<int16_t>::max();
 
 // Linear threshold
-constexpr double linear_threshold = 3;
+constexpr double linear_threshold = 10;
 // Angular threshold
 constexpr double angular_threshold = 2;
 // Angular intermediate threshold (when the robot turns on itself to go straight to its destination)
