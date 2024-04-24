52dd9cf03bdff26803635b95da3e341bd2499eef
Gilles DOFFE <g.doffe@gmail.com>
2024-04-24 21:37:04 +0200

    applications/cup2024-robot-motion-control: tune parameters
    As robot wheels have been changed for Banebots wheels 40° A shore, it
    implies new calibration and thus new settings.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- applications/cup2024-robot-motion-control/include/real/app_conf.hpp
+++ applications/cup2024-robot-motion-control/include/real/app_conf.hpp
@@ -12,11 +12,11 @@ constexpr double angular_pose_pid_kp = 0.1;
 constexpr double angular_pose_pid_ki = 0;
 constexpr double angular_pose_pid_kd = 0;
 // Linear speed PID
-constexpr double linear_speed_pid_kp = 30;
+constexpr double linear_speed_pid_kp = 25;
 constexpr double linear_speed_pid_ki = 5;
 constexpr double linear_speed_pid_kd = 0;
 // Angular speed PID
-constexpr double angular_speed_pid_kp = 50;
+constexpr double angular_speed_pid_kp = 75;
 constexpr double angular_speed_pid_ki = 3;
 constexpr double angular_speed_pid_kd = 0;
 
@@ -32,7 +32,7 @@ constexpr double angular_speed_pid_integral_limit = etl::numeric_limits<int16_t>
 // Linear threshold
 constexpr double linear_threshold = 1;
 // Angular threshold
-constexpr double angular_threshold = 0.25;
+constexpr double angular_threshold = 1;
 // Angular intermediate threshold (when the robot turns on itself to go straight to its destination)
-constexpr double angular_intermediate_threshold = 2;
+constexpr double angular_intermediate_threshold = 5;
 
