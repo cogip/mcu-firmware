5ba94d7b62c682b98c0576187c904cf5cdda09ca
Gilles DOFFE <g.doffe@gmail.com>
2024-05-05 22:33:31 +0200

    platforms/pf-robot-motors: align state message behavior on pf-robot-actuators
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- platforms/pf-robot-motors/Motor.cpp
+++ platforms/pf-robot-motors/Motor.cpp
@@ -15,8 +15,9 @@ namespace positional_actuators {
 
 /// Constructor
 Motor::Motor(
-        Enum id,
+        cogip::pf::actuators::Enum id,
         uint32_t default_timeout_period,
+        send_state_cb_t send_state_cb,
         motor_driver_t *motor_driver,
         uint8_t motor_id,
         gpio_t clear_overload_pin,
@@ -27,7 +28,7 @@ Motor::Motor(
         cogip::motion_control::SpeedFilterParameters *speed_filter_parameters,
         cogip::motion_control::motor_get_speed_and_pose_cb_t motor_get_speed_and_pose_cb,
         cogip::motion_control::motor_process_commands_cb_t motor_process_commands_cb
-    ) : PositionalActuator(id, default_timeout_period),
+    ) : PositionalActuator(id, default_timeout_period, send_state_cb),
         motor_driver_(motor_driver),
         motor_id_(motor_id),
         clear_overload_pin_(clear_overload_pin),
