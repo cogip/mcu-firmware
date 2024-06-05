e9fd759ebd4a6e8607d6c8f8a83413f0bd385640
Gilles DOFFE <g.doffe@gmail.com>
2024-06-05 23:12:00 +0200

    platforms/pf-pami: reset encoders when resetting motion control
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- platforms/pf-pami/motion_control.cpp
+++ platforms/pf-pami/motion_control.cpp
@@ -424,6 +424,8 @@ void pf_motion_control_reset(void)
 
     // Reset pose straight filter state
     pose_straight_filter.reset_current_state();
+
+    pf_encoder_reset();
 }
 
 void pf_disable_motion_control()
