9f5764cfdadb4e5feaa0ce3360b7b20186ceae64
Mathis Lécrivain <lecrivain.mathis@gmail.com>
2024-11-08 15:06:00 +0100

    lib: add motor and motor dirver interfaces
    Add motor interface that provide APIs to:
    - enable
    - disable
    - brake
    - set speed between -100 to 100 percent
    
    Add motor driver interface that provide APIs to:
    - initialize the driver
    - reset the driver
    
    Signed-off-by: Mathis Lécrivain <lecrivain.mathis@gmail.com>

---
+++ lib/motor/include/motor/MotorDriverInterface.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+namespace cogip {
+
+namespace motor {
+
+class MotorDriverInterface {
+public:
+    /// @brief Virtual destructor
+    ~MotorDriverInterface()
+    {
+    }
+
+    /// @brief Intialize the motor driver
+    /// @return 0 on success, negative on error
+    virtual int init() = 0;
+    
+    /// @brief Reset the motor driver
+    /// @return 0 on success, negative on error
+    virtual int reset() = 0;
+    
+    /// @brief Enable the motor
+    /// @param id id of the motor
+    /// @return 0 on success, negative on error
+    virtual int enable(int id) = 0;
+
+    /// @brief Disable the motor
+    /// @param id id of the motor
+    /// @return 0 on success, negative on error
+    virtual int disable(int id) = 0;
+
+    /// @brief Set motor speed
+    /// @param speed speed in % [-100; 100]
+    /// @param id id of the motor
+    /// @return  0 on success, negative on error
+    virtual int speed(int speed, int id) = 0;
+    
+    /// @brief break the motor
+    /// @param id id of the motor
+    /// @return 0 on success, negative on error
+    virtual int brake(int id) = 0;
+};
+
+} // namespace motor
+
+} // namespace cogip
+
+// @}
---
+++ lib/motor/include/motor/MotorInterface.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+namespace cogip {
+
+namespace motor {
+
+class MotorInterface {
+public:
+    /// @brief Virtual destructor
+    ~MotorInterface()
+    {
+    }
+
+    /// @brief Enable the motor
+    /// @return 0 on success, negative on error
+    virtual int enable() = 0;
+
+    /// @brief Enable the motor
+    /// @return 0 on success, negative on error
+    virtual int disable() = 0;
+
+    /// @brief Set motor speed
+    /// @param speed speed in % [-100; 100]
+    /// @return  0 on success, negative on error
+    virtual int speed(int speed) = 0;
+    
+    /// @brief break the motor
+    /// @return 0 on success, negative on error
+    virtual int brake() = 0;
+};
+
+} // namespace motor
+
+} // namespace cogip
+
+// @}
