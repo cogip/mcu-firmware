6aa8f7ec0d62dea62cee564329313735fe30c8b6
Mathis Lécrivain <lecrivain.mathis@gmail.com>
2024-12-05 13:10:54 +0100

    drive controller: introduce drive controller interface
    The purpose of this class is to define a common interface used by
    platform engine in order to use any kind of drive controller (eg.
    DifferentialDrive or HolonomicDrive)
    
    This interface provides one method to set polar robot velocity.
    
    Signed-off-by: Mathis Lécrivain <lecrivain.mathis@gmail.com>

---
+++ motion_control/drive_controller/include/drive_controller/DriveControllerInterface.hpp
@@ -0,0 +1,34 @@
+// Copyright (C) 2024 COGIP Robotics association <cogip35@gmail.com>
+// This file is subject to the terms and conditions of the GNU Lesser
+// General Public License v2.1. See the file LICENSE in the top level
+// directory for more details.
+
+/// @brief       Drive controller interface
+/// @author      Mathis Lécrivain <lecrivain.mathis@gmail.com>
+
+#pragma once
+
+#include "cogip_defs/Polar.hpp"
+
+namespace cogip {
+
+namespace drive_controller {
+
+class DriveControllerInterface {
+public:
+    /// @brief Virtual destructor
+    ~DriveControllerInterface()
+    {
+    }
+
+    /// @brief Set polar velocity command
+    /// @param command polar velocity command reference
+    /// @return  0 on success, negative on error
+    virtual int set_polar_velocity(cogip_defs::Polar &command) = 0;
+};
+
+} // namespace drive_controller
+
+} // namespace cogip
+
+// @}
