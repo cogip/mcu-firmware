2f84ebe67ba4632fb91ee0df0ca5c8a0b87c92c5
Mathis Lécrivain <lecrivain.mathis@gmail.com>
2024-12-12 22:44:40 +0100

    WIP: 166-power-supply-inputs-signals-monitoring
    Signed-off-by: Mathis Lécrivain <lecrivain.mathis@gmail.com>

---
+++ platforms/pf-power-supply/pf_power_supply.cpp
@@ -0,0 +1,116 @@
+///  Copyright (C) 2024 COGIP Robotics association
+///
+///  This file is subject to the terms and conditions of the GNU Lesser
+///  General Public License v2.1. See the file LICENSE in the top level
+///  directory for more details.
+
+///  @ingroup     platforms
+///  @brief       power supply platform
+///
+///  @author      Mathis LECRIVAIN <lecrivain.mathis@gmail.com>
+
+// Standard includes
+#include <cstdint>
+
+// RIOT includes
+#include <event.h>
+#include <ztimer.h>
+
+// Platform includes
+#include "pf_power_supply.hpp"
+
+// Libraries includes
+#include "gpio/gpio.hpp"
+
+// Embedded Template Library includes
+#include "etl/map.h"
+
+// Protobuf messages
+#include "PB_Power_supply.hpp"
+
+namespace cogip
+{
+namespace pf
+{
+namespace power_supply
+{
+    
+/// GPIOs handler thread stack
+static char _gpio_handling_thread_stack[THREAD_STACKSIZE_DEFAULT];
+
+static void pxVx_error_cb_(void *arg);
+
+// PGood GPIO objects creation
+static gpio::GPIO p3V3_pgood_gpio_(pf::power_supply::P3V3_PGOOD_PIN, GPIO_IN, GPIO_FALLING, pxVx_error_cb_, NULL);
+static gpio::GPIO p5V0_pgood_gpio_(pf::power_supply::P5V0_PGOOD_PIN, GPIO_IN, GPIO_FALLING, pxVx_error_cb_, NULL);
+static gpio::GPIO p7V5_pgood_gpio_(pf::power_supply::P7V5_PGOOD_PIN, GPIO_IN, GPIO_FALLING, pxVx_error_cb_, NULL);
+static gpio::GPIO pxVx_pgood_gpio_(pf::power_supply::PxVx_PGOOD_PIN, GPIO_IN, GPIO_FALLING, pxVx_error_cb_, NULL);
+
+// Map protobuf PGood states enum to it's GPIO objects
+static etl::map<PB_PGoodTypeEnum, const gpio::GPIO*, pf::power_supply::PGOOD_PIN_NUMBER> pgood_gpios_{
+    etl::pair{PB_PGoodTypeEnum::P3V3_PGOOD, &p3V3_pgood_gpio_},
+    etl::pair{PB_PGoodTypeEnum::P5V0_PGOOD, &p5V0_pgood_gpio_},
+    etl::pair{PB_PGoodTypeEnum::P7V5_PGOOD, &p7V5_pgood_gpio_},
+    etl::pair{PB_PGoodTypeEnum::PxVx_PGOOD, &pxVx_pgood_gpio_}
+};
+
+static void high_power_state_cb_(void *arg);
+
+static gpio::GPIO en_high_power_gpio_(pf::power_supply::EN_HIGH_POWER_PIN, GPIO_IN, GPIO_BOTH, high_power_state_cb_, NULL);
+static gpio::GPIO battery_valid_n_gpio_(pf::power_supply::BATTERY_VALID_N_PIN, GPIO_IN);
+static gpio::GPIO dc_supply_valid_n_gpio_(pf::power_supply::DC_SUPPLY_VALID_N_PIN, GPIO_IN);
+
+static void *_gpio_handling_thread(void *args);
+
+/// @brief 
+/// @param arg 
+static void pxVx_error_cb_([[maybe_unused]]void *arg)
+{
+    
+}
+
+/// @brief 
+/// @param arg 
+static void high_power_state_cb_([[maybe_unused]]void *arg)
+{
+    
+}
+
+static void *_gpio_handling_thread([[maybe_unused]]void *args)
+{
+    //(void)args;
+    //event_t *event;
+
+    //// Initialize GPIOs event queue
+    //event_queue_init(&_new_gpio_event_queue);
+
+    //while ((event = event_wait(&_new_gpio_event_queue))) {
+    //    gpio_t pin = _gpio_pins[event];
+
+    //    BoolSensor* bool_sensor = _bool_sensors_events[event];
+    //    if (bool_sensor) {
+    //        bool_sensor->set_state(!gpio_read(pin));
+    //        bool_sensor->send_state();
+    //    }
+    //}
+
+    return nullptr;
+}
+
+void pf_init_power_supply(void)
+{
+    // TODO
+}
+
+void pf_init_power_supply_tasks(void)
+{
+    // Power supply GPIO handling thread
+    thread_create(_gpio_handling_thread_stack, sizeof(_gpio_handling_thread_stack), THREAD_PRIORITY_MAIN - 1,
+        THREAD_CREATE_STACKTEST, _gpio_handling_thread, NULL, "GPIO handling thread");
+}
+
+} // namespace power_supply
+} // namespace pf
+} // namespace cogip
+
+/// @}
--- platforms/pf-power-supply/platform.cpp
+++ platforms/pf-power-supply/platform.cpp
@@ -9,17 +9,11 @@
 #include "canpb/ReadBuffer.hpp"
 #include "platform.hpp"
 #include "utils.hpp"
-#include "gpio.hpp"
-
-/* Embedded Template Library includes */
-#include "etl/pool.h"
-#include "etl/map.h"
 
 #define ENABLE_DEBUG (0)
 #include "debug.h"
 
 #include "pf_power_supply.hpp"
-#include "PB_Power_supply.hpp"
 
 // canpb CAN device
 static cogip::canpb::CanProtobuf canpb(0);
@@ -38,41 +32,37 @@ constexpr uint16_t heartbeat_leds_interval = 200;
 // Heartbeat thread period
 constexpr uint16_t heartbeat_period = 1800;
 
-static etl::pool<cogip::gpio::GPIO, 4> _gpio_pool;
-
-static etl::map<PB_PGoodTypeEnum, cogip::gpio::GPIO *, 4> _pgood_gpios;
-
 static void *_heartbeat_thread(void *args)
 {
-	(void)args;
+    (void)args;
 
-	// Init loop iteration start time
-	ztimer_now_t loop_start_time = ztimer_now(ZTIMER_MSEC);
+    // Init loop iteration start time
+    ztimer_now_t loop_start_time = ztimer_now(ZTIMER_MSEC);
 
-	while (true) {
-		gpio_set(HEARTBEAT_LED);
-		ztimer_sleep(ZTIMER_MSEC, heartbeat_leds_interval);
-		gpio_clear(HEARTBEAT_LED);
-		ztimer_sleep(ZTIMER_MSEC, heartbeat_leds_interval);
-		gpio_set(HEARTBEAT_LED);
-		ztimer_sleep(ZTIMER_MSEC, heartbeat_leds_interval);
-		gpio_clear(HEARTBEAT_LED);
+    while (true) {
+        gpio_set(HEARTBEAT_LED);
+        ztimer_sleep(ZTIMER_MSEC, heartbeat_leds_interval);
+        gpio_clear(HEARTBEAT_LED);
+        ztimer_sleep(ZTIMER_MSEC, heartbeat_leds_interval);
+        gpio_set(HEARTBEAT_LED);
+        ztimer_sleep(ZTIMER_MSEC, heartbeat_leds_interval);
+        gpio_clear(HEARTBEAT_LED);
 
-		// Wait thread period to end
-		ztimer_periodic_wakeup(ZTIMER_MSEC, &loop_start_time, heartbeat_period);
-	}
+        // Wait thread period to end
+        ztimer_periodic_wakeup(ZTIMER_MSEC, &loop_start_time, heartbeat_period);
+    }
 
-	return 0;
+    return 0;
 }
 
 void pf_set_copilot_connected(bool connected)
 {
-	copilot_connected = connected;
+    copilot_connected = connected;
 }
 
 cogip::canpb::CanProtobuf &pf_get_canpb()
 {
-	return canpb;
+    return canpb;
 }
 
 /// Start game message handler
@@ -92,52 +82,46 @@ static void _handle_game_end([[maybe_unused]] cogip::canpb::ReadBuffer &buffer)
 
 void _handle_copilot_connected(cogip::canpb::ReadBuffer &)
 {
-	pf_set_copilot_connected(true);
-	std::cout << "Copilot connected" << std::endl;
+    pf_set_copilot_connected(true);
+    std::cout << "Copilot connected" << std::endl;
 }
 
 void _handle_copilot_disconnected(cogip::canpb::ReadBuffer &)
 {
-	pf_set_copilot_connected(false);
-	std::cout << "Copilot disconnected" << std::endl;
+    pf_set_copilot_connected(false);
+    std::cout << "Copilot disconnected" << std::endl;
 }
 
 void pf_init(void)
 {
-	thread_create(heartbeat_thread_stack, sizeof(heartbeat_thread_stack), THREAD_PRIORITY_MAIN - 1,
-		      THREAD_CREATE_STACKTEST, _heartbeat_thread, NULL, "Heartbeat thread");
+    thread_create(heartbeat_thread_stack, sizeof(heartbeat_thread_stack), THREAD_PRIORITY_MAIN - 1,
+              THREAD_CREATE_STACKTEST, _heartbeat_thread, NULL, "Heartbeat thread");
 
-	/* Create and map gpios with PB enum id */
-	_pgood_gpios[PB_PGoodTypeEnum::P3V3_PGOOD] = _gpio_pool.create(P3V3_PGOOD_PIN, GPIO_IN);
-	_pgood_gpios[PB_PGoodTypeEnum::P5V0_PGOOD] = _gpio_pool.create(P5V0_PGOOD_PIN, GPIO_IN);
-	_pgood_gpios[PB_PGoodTypeEnum::P7V5_PGOOD] = _gpio_pool.create(P7V5_PGOOD_PIN, GPIO_IN);
-	_pgood_gpios[PB_PGoodTypeEnum::PxVx_PGOOD] = _gpio_pool.create(PxVx_PGOOD_PIN, GPIO_IN);
+    /* Initialize CANPB */
+    int canpb_res = canpb.init(&canpb_filter);
+    if (canpb_res) {
+        COGIP_DEBUG_CERR("CAN initialization failed, error: " << canpb_res);
+    } else {
+        canpb.register_message_handler(game_reset_uuid,
+                           cogip::canpb::message_handler_t::create<_handle_game_reset>());
+        canpb.register_message_handler(game_start_uuid,
+                           cogip::canpb::message_handler_t::create<_handle_game_start>());
+        canpb.register_message_handler(game_end_uuid,
+                           cogip::canpb::message_handler_t::create<_handle_game_end>());
+        canpb.register_message_handler(copilot_connected_uuid,
+                           cogip::canpb::message_handler_t::create<_handle_copilot_connected>());
+        canpb.register_message_handler(copilot_disconnected_uuid,
+                           cogip::canpb::message_handler_t::create<_handle_copilot_disconnected>());
 
-	for (aut) {
+        canpb.start_reader();
+    }
 
-		/* Initialize CANPB */
-		int canpb_res = canpb.init(&canpb_filter);
-	}
-	if (canpb_res) {
-		COGIP_DEBUG_CERR("CAN initialization failed, error: " << canpb_res);
-	} else {
-		canpb.register_message_handler(game_reset_uuid,
-					       cogip::canpb::message_handler_t::create<_handle_game_reset>());
-		canpb.register_message_handler(game_start_uuid,
-					       cogip::canpb::message_handler_t::create<_handle_game_start>());
-		canpb.register_message_handler(game_end_uuid,
-					       cogip::canpb::message_handler_t::create<_handle_game_end>());
-		canpb.register_message_handler(copilot_connected_uuid,
-					       cogip::canpb::message_handler_t::create<_handle_copilot_connected>());
-		canpb.register_message_handler(copilot_disconnected_uuid,
-					       cogip::canpb::message_handler_t::create<_handle_copilot_disconnected>());
-
-		canpb.start_reader();
-	}
-
-	shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+    
+    cogip::pf::power_supply::pf_init_power_supply();
 }
 
 void pf_init_tasks(void)
 {
+    cogip::pf::power_supply::pf_init_power_supply_tasks();
 }
