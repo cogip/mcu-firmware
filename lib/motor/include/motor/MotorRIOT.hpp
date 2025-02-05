51d7be6ee737f130e0845536e92d7c481271873d
Mathis Lécrivain <lecrivain.mathis@gmail.com>
2025-02-05 21:32:22 +0100

    lib: use double for motor class for finest speed control
    In order to use the full pwm resolution and be able to use the full
    range of configured PWM resolution, we need to use floatting point
    numbers for motor speed control.
    
    Signed-off-by: Mathis Lécrivain <lecrivain.mathis@gmail.com>

--- lib/motor/include/motor/MotorRIOT.hpp
+++ lib/motor/include/motor/MotorRIOT.hpp
@@ -7,44 +7,44 @@ namespace cogip {
 
 namespace motor {
 
-class MotorRIOT: public MotorInterface {
-public:
-    MotorRIOT(MotorDriverRIOT& driver, int id): driver_(driver), id_(id)
-    {
-    }
+class MotorRIOT : public MotorInterface {
+      public:
+	MotorRIOT(MotorDriverRIOT &driver, int id) : driver_(driver), id_(id)
+	{
+	}
 
-    /// @brief Enable the motor
-    /// @return 0 on success, negative on error
-    int enable()
-    {
-        return driver_.enable(id_);
-    }
+	/// @brief Enable the motor
+	/// @return 0 on success, negative on error
+	int enable()
+	{
+		return driver_.enable(id_);
+	}
 
-    /// @brief Enable the motor
-    /// @return 0 on success, negative on error
-    int disable()
-    {
-        return driver_.disable(id_);
-    }
+	/// @brief Enable the motor
+	/// @return 0 on success, negative on error
+	int disable()
+	{
+		return driver_.disable(id_);
+	}
 
-    /// @brief Set motor speed
-    /// @param speed speed in % [-100; 100]
-    /// @return  0 on success, negative on error
-    int speed(int speed)
-    {
-        return driver_.speed(speed, id_);
-    }
-    
-    /// @brief break the motor
-    /// @return 0 on success, negative on error
-    int brake()
-    {
-        return driver_.brake(id_);
-    }
+	/// @brief Set motor speed
+	/// @param speed speed in % [-100; 100]
+	/// @return  0 on success, negative on error
+	int speed(double speed)
+	{
+		return driver_.speed(speed, id_);
+	}
 
-private:
-    MotorDriverRIOT &driver_;
-    int id_;
+	/// @brief break the motor
+	/// @return 0 on success, negative on error
+	int brake()
+	{
+		return driver_.brake(id_);
+	}
+
+      private:
+	MotorDriverRIOT &driver_;
+	int id_;
 };
 
 } // namespace motor
