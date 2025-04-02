ce142a6e505a4cc600fdf2b04fb299eb90b5ffe9
Gilles DOFFE <g.doffe@gmail.com>
2025-04-02 14:54:24 +0200

    applications/robot-motors: tune speed PID for 2025
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- applications/cup2024-robot-motors/include/real/app_conf.hpp
+++ applications/cup2024-robot-motors/include/real/app_conf.hpp
@@ -4,18 +4,19 @@
 #include "etl/numeric.h"
 
 // Motor lift pose PID
-constexpr float motor_lift_pose_pid_kp = 1;
+constexpr float motor_lift_pose_pid_kp = .2;
 constexpr float motor_lift_pose_pid_ki = 0;
 constexpr float motor_lift_pose_pid_kd = 0;
 // Motor lift speed PID
-constexpr float motor_lift_speed_pid_kp = 200;
-constexpr float motor_lift_speed_pid_ki = 75;
+constexpr float motor_lift_speed_pid_kp = 15;
+constexpr float motor_lift_speed_pid_ki = 7.5;
 constexpr float motor_lift_speed_pid_kd = 0;
 
 // Motor lift pose PID integral limit
 constexpr float motor_lift_pose_pid_integral_limit = etl::numeric_limits<int16_t>::max();
 // Motor lift speed PID integral limit
-constexpr float motor_lift_speed_pid_integral_limit = 500 / 75; // (PWM resolution / Ki)
+//constexpr float motor_lift_speed_pid_integral_limit = etl::numeric_limits<int16_t>::max();
+constexpr float motor_lift_speed_pid_integral_limit = 500 / motor_lift_speed_pid_ki;
 
 // Motor lift threshold
 constexpr float motor_lift_threshold = 0.1;
