d68d3246561640c855603db9542b02010f7791e8
Gilles DOFFE <g.doffe@gmail.com>
2025-05-02 11:45:03 +0200

    platforms/pf-robot-motors: clean and use new actuator library
    * Remove redundant code with new actuator library like actuator sources
      and headers (Motor.cpp, PositionalActuator.cpp, custom protobuf, etc.)
    * Link against the new actuator library.
    * Delegate configuration to the application: remove out any
      application‑specific parameter defaults so each app defines its own
      actuator settings externally.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- platforms/pf-robot-motors/include/pf_positional_actuators.hpp
+++ platforms/pf-robot-motors/include/pf_positional_actuators.hpp
@@ -11,124 +11,47 @@
 
 #pragma once
 
-// Project includes
-#include "board.h"
-#include "PositionalActuator.hpp"
-#include "trigonometry.h"
-
-// RIOT includes
-#include <periph/gpio.h>
+#include "actuator/PositionalActuator.hpp"
+#include "actuator/LiftParameters.hpp"
 
 namespace cogip {
 namespace pf {
 namespace actuators {
 namespace positional_actuators {
 
-/// Quadrature decoding
-#ifndef QDEC_MODE
-#define QDEC_MODE                   QDEC_X4
-#endif
-#define QDEC_BOTTOM_LIFT_POLARITY   -1
-#define QDEC_TOP_LIFT_POLARITY      1
-
-/// Motor minimal PWM
-constexpr int pwm_minimal = 70;
-
-/// @name Lift motor application properties
-///
-/// To be computed:
-///  - pulse_per_mm             : number of pulses per mm of coding wheel
-///
-/// must be known:
-///  - wheels_diameter          : coding wheel diameter (mm)
-///  - wheels_encoder_resolution: number of pulses by turn of coding wheels
-///
-/// Intermediate calculation:
-///  - wheels_perimeter = pi*wheels_diameter
-///  - pulse_per_mm = wheels_encoder_resolution / wheels_perimeter
-///
-/// @{
-constexpr float wheels_diameter_mm = 12.03;
-constexpr float wheels_encoder_resolution = 37.35 * 11 * 4;
-constexpr float wheels_perimeter_mm = M_PI * wheels_diameter_mm;
-constexpr float pulse_per_mm = wheels_encoder_resolution / wheels_perimeter_mm;///< WHEELS_ENCODER_RESOLUTION / WHEELS_PERIMETER
-/// @}
-
-///< controller thread loop period
-constexpr uint16_t motor_lift_control_thread_period_ms = 20;
-
-/// Lift motors speed filter parameters
-constexpr float motor_lift_anti_blocking_speed_threshold_per_period = 0.3;
-constexpr float motor_lift_anti_blocking_error_threshold_per_period = 0.02;
-constexpr float motor_lift_anti_blocking_blocked_cycles_nb_threshold = 10;
-constexpr float motor_lift_min_speed_motor_lift_m_per_s = 0;
-constexpr float motor_lift_max_init_speed_motor_lift_m_per_s = 0.02;
-constexpr float motor_lift_max_speed_motor_lift_m_per_s = 0.17;
-constexpr float motor_lift_max_acc_motor_lift_m_per_s2 = motor_lift_max_speed_motor_lift_m_per_s * 10;
-constexpr float motor_lift_max_dec_motor_lift_m_per_s2 = motor_lift_max_speed_motor_lift_m_per_s * 2;
-constexpr float motor_lift_max_acc_motor_lift_mm_per_period2 = (
-    (1000 * motor_lift_max_acc_motor_lift_m_per_s2 * motor_lift_control_thread_period_ms * motor_lift_control_thread_period_ms) \
-    / (1000 * 1000)
-    );          ///< Maximum motor_lift acceleration (mm/<motor_lift_control_thread_period_ms>²)
-constexpr float motor_lift_max_dec_motor_lift_mm_per_period2 = (
-    (1000 * motor_lift_max_dec_motor_lift_m_per_s2 * motor_lift_control_thread_period_ms * motor_lift_control_thread_period_ms) \
-    / (1000 * 1000)
-    );          ///< Maximum motor_lift deceleration (mm/<motor_lift_control_thread_period_ms>²)
-constexpr float motor_lift_min_speed_motor_lift_mm_per_period = (
-    (1000 * motor_lift_min_speed_motor_lift_m_per_s * motor_lift_control_thread_period_ms) \
-    / 1000);    ///< Minimum motor_lift speed (mm/<motor_lift_control_thread_period_ms>)
-constexpr float motor_lift_max_speed_motor_lift_mm_per_period = (
-    (1000 * motor_lift_max_speed_motor_lift_m_per_s * motor_lift_control_thread_period_ms) \
-    / 1000);    ///< Maximum motor_lift speed (mm/<motor_lift_control_thread_period_ms>)
-constexpr float motor_lift_max_init_speed_motor_lift_mm_per_period = (
-    (1000 * motor_lift_max_init_speed_motor_lift_m_per_s * motor_lift_control_thread_period_ms) \
-    / 1000);    ///< Maximum motor_lift init speed (mm/<motor_lift_control_thread_period_ms>)
-
-// Motors ids
-constexpr auto START_LINE = __LINE__;
-enum class Enum: uint8_t {
-    MOTOR_BOTTOM_LIFT = 0,
-    MOTOR_TOP_LIFT = 1,
-};
-constexpr auto COUNT = __LINE__ - START_LINE - 3;
-
-/// Actuators timeouts
-/// @{
-constexpr uint32_t default_timeout_period_motor_bottom_lift = 30;
-constexpr uint32_t default_timeout_period_motor_top_lift = 40;
-/// @}
-
-/// Motors initial pose
-/// @{
-constexpr int32_t motor_bottom_lift_initial_pose = 5;
-constexpr int32_t motor_top_lift_initial_pose = 20;
-/// @}
-
-/// Actuators DC motors IDs
-/// @{
-/// @}
-
 /// Initialize positional_actuators.
 void init();
 
+/// Functional initiliazation sequence
+void init_sequence();
+
 /// Check if a positional_actuator identified by id exists.
 bool contains(
-    cogip::pf::actuators::Enum id  ///< [in] positional_actuator id
+    cogip::actuators::Enum id  ///< [in] positional_actuator id
 );
 
 /// Get a positional_actuator by id.
-PositionalActuator & get(
-    cogip::pf::actuators::Enum id  ///< [in] positional_actuator id
+cogip::actuators::positional_actuators::PositionalActuator & get(
+    cogip::actuators::Enum id  ///< [in] positional_actuator id
 );
 
 /// Disable all positional actuators
 void disable_all();
 
+/// Enable all positional actuators
+void enable_all();
+
 /// Send positional actuator state protobuf message
-void send_state(cogip::pf::actuators::Enum positional_actuator);
+void send_state(cogip::actuators::Enum positional_actuator);
 
 /// Initialize motors at their origin
 void pf_init_motors_sequence(void);
+
+/// Instanciate a lift
+/// @return 0 if ok, negative otherwise
+int create_lift(
+    cogip::actuators::Enum id,
+    const cogip::actuators::positional_actuators::LiftParameters& lift_params);
 
 } // namespace positional_actuators
 } // namespace actuators
