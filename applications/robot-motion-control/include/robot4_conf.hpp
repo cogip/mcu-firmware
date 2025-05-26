74ba7bb40d891d7fbbf5847b10e1ae10593ce945
Gilles DOFFE <g.doffe@gmail.com>
2025-05-27 00:46:05 +0200

    applications/robot-motion-control: tune robot2 to robot5 parameters
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- applications/robot-motion-control/include/robot4_conf.hpp
+++ applications/robot-motion-control/include/robot4_conf.hpp
@@ -15,68 +15,68 @@
 
 /// Motors properties
 constexpr float motor_wheels_diameter_mm = 60.0;
-constexpr float motor_wheels_distance_mm = 88.0;
+constexpr float motor_wheels_distance_mm = 89.5;
 constexpr float left_motor_constant = 7.079;
 constexpr float right_motor_constant = 7.079;
 
-constexpr float min_motor_speed_percent = 10;
+constexpr float min_motor_speed_percent = 0;
 constexpr float max_motor_speed_percent = 100;
 
 /// Encoders properties
-constexpr float left_encoder_wheels_diameter_mm = 60.0;
-constexpr float right_encoder_wheels_diameter_mm = 60.0;
-constexpr float encoder_wheels_distance_mm = 88.0;
+constexpr float left_encoder_wheels_diameter_mm = motor_wheels_diameter_mm;
+constexpr float right_encoder_wheels_diameter_mm = motor_wheels_diameter_mm;
+constexpr float encoder_wheels_distance_mm = motor_wheels_distance_mm;
 constexpr float encoder_wheels_resolution_pulses = 16 * 4 * 8;
 
 // Linear pose PID
-constexpr float linear_pose_pid_kp = 1.0;
-constexpr float linear_pose_pid_ki = 0;
-constexpr float linear_pose_pid_kd = 0;
+constexpr float linear_pose_pid_kp = 1;
+constexpr float linear_pose_pid_ki = 0.0;
+constexpr float linear_pose_pid_kd = 0.0;
 // Angular pose PID
-constexpr float angular_pose_pid_kp = 1.0;
-constexpr float angular_pose_pid_ki = 0;
-constexpr float angular_pose_pid_kd = 0;
+constexpr float angular_pose_pid_kp = 1;
+constexpr float angular_pose_pid_ki = 0.0;
+constexpr float angular_pose_pid_kd = 0.0;
 // Linear speed PID
-constexpr float linear_speed_pid_kp = 1.0;
-constexpr float linear_speed_pid_ki = 0.0;
-constexpr float linear_speed_pid_kd = 0;
+constexpr float linear_speed_pid_kp = 10;
+constexpr float linear_speed_pid_ki = 1;
+constexpr float linear_speed_pid_kd = 0.0;
 // Angular speed PID
-constexpr float angular_speed_pid_kp = 1.0;
-constexpr float angular_speed_pid_ki = 0.0;
-constexpr float angular_speed_pid_kd = 0;
+constexpr float angular_speed_pid_kp = 10;
+constexpr float angular_speed_pid_ki = 1;
+constexpr float angular_speed_pid_kd = 0.0;
+
+// Linear threshold
+constexpr float linear_threshold = 5;
+// Angular threshold
+constexpr float angular_threshold = 5;
+// Angular intermediate threshold (when the robot turns on itself to go straight to its destination)
+constexpr float angular_intermediate_threshold = 5;
+
+// TODO:
+constexpr double platform_linear_anti_blocking_speed_threshold_mm_per_s = 12.5;
+constexpr double platform_linear_anti_blocking_error_threshold_mm_per_s = 50;
+constexpr double platform_linear_anti_blocking_blocked_cycles_nb_threshold = 10;
+
+constexpr float min_speed_mm_per_s = 0;    ///< Minimum speed (mm/s)
+constexpr float max_speed_mm_per_s = 1000.0;  ///< Maximum speed (mm/s)
+constexpr float max_acc_mm_per_s2 = 750.0; ///< Maximum acceleration (mm/s²)
+constexpr float max_dec_mm_per_s2 = 1000.0; ///< Maximum deceleration (mm/s²)
+
+constexpr float min_speed_deg_per_s = 20;  ///< Maximum speed (deg/s)
+constexpr float max_speed_deg_per_s = 360; ///< Maximum speed (deg/s)
+constexpr float max_acc_deg_per_s2 = 180;  ///< Maximum acceleration (deg/s²)
+constexpr float max_dec_deg_per_s2 = 360;  ///< Maximum deceleration (deg/s²)
 
 // Linear pose PID integral limit
 constexpr float linear_pose_pid_integral_limit = etl::numeric_limits<int16_t>::max();
 // Angular pose PID integral limit
 constexpr float angular_pose_pid_integral_limit = etl::numeric_limits<int16_t>::max();
 // Linear speed PID integral limit
-constexpr float linear_speed_pid_integral_limit = etl::numeric_limits<int16_t>::max();
+constexpr float linear_speed_pid_integral_limit = max_speed_mm_per_s / linear_speed_pid_ki;
 // Angular speed PID integral limit
-constexpr float angular_speed_pid_integral_limit = etl::numeric_limits<int16_t>::max();
-
-// Linear threshold
-constexpr float linear_threshold = 1;
-// Angular threshold
-constexpr float angular_threshold = 1;
-// Angular intermediate threshold (when the robot turns on itself to go straight to its destination)
-constexpr float angular_intermediate_threshold = 5;
-
-// TODO:
-constexpr float platform_linear_anti_blocking_speed_threshold_per_period = 0.25;
-constexpr float platform_linear_anti_blocking_error_threshold_per_period = 1;
-constexpr float platform_linear_anti_blocking_blocked_cycles_nb_threshold = 15;
-
-constexpr float min_speed_mm_per_s = 0;    ///< Minimum speed (mm/s)
-constexpr float max_speed_mm_per_s = 500;  ///< Maximum speed (mm/s)
-constexpr float max_acc_mm_per_s2 = 500.0; ///< Maximum acceleration (mm/s²)
-constexpr float max_dec_mm_per_s2 = 500.0; ///< Maximum deceleration (mm/s²)
-
-constexpr float min_speed_deg_per_s = 0;  ///< Maximum speed (deg/s)
-constexpr float max_speed_deg_per_s = 60; ///< Maximum speed (deg/s)
-constexpr float max_acc_deg_per_s2 = 30;  ///< Maximum acceleration (deg/s²)
-constexpr float max_dec_deg_per_s2 = 30;  ///< Maximum deceleration (deg/s²)
+constexpr float angular_speed_pid_integral_limit = max_speed_deg_per_s / angular_speed_pid_ki;
 
 // Linear antiblocking
-constexpr bool platform_linear_antiblocking = true;
+constexpr bool platform_linear_antiblocking = false;
 // Angular antiblocking
 constexpr bool angular_antiblocking = false;
