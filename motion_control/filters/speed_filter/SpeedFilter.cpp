411b1a2ba7ca70a80bd05cecfb6d395be9237c72
Gilles DOFFE <g.doffe@gmail.com>
2025-12-04 14:34:11 +0100

    motion_control: simplify SpeedFilter, move anti-blocking to dedicated controller
    SpeedFilter was doing too much: speed limiting and anti-blocking detection.
    The anti-blocking logic has been moved to a dedicated AntiBlockingController,
    following the single responsibility principle. This makes each controller
    easier to test, configure independently, and reuse in different pipeline
    configurations.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- motion_control/filters/speed_filter/SpeedFilter.cpp
+++ motion_control/filters/speed_filter/SpeedFilter.cpp
@@ -1,11 +1,9 @@
 // System includes
-#include "etl/absolute.h"
 #include "etl/algorithm.h"
 #include <cmath>
 
 // RIOT includes
 #include "log.h"
-#include <inttypes.h>
 
 // Project includes
 #include "speed_filter/SpeedFilter.hpp"
@@ -50,72 +48,32 @@ void SpeedFilter::limit_speed_order(float* speed_order, float raw_target, float 
 
 void SpeedFilter::execute(ControllersIO& io)
 {
-    DEBUG("Execute SpeedFilter\n");
-
     // Read commanded speed before filtering (default to zero if missing)
     float speed_order = 0.0f;
     if (auto opt = io.get_as<float>(keys_.speed_order)) {
         speed_order = *opt;
-    } else {
-        LOG_WARNING("WARNING: %s is not available, using default value %f\n",
-                    keys_.speed_order.data(), speed_order);
     }
 
     // Read measured current speed (default to zero if missing)
     float current_speed = 0.0f;
     if (auto opt = io.get_as<float>(keys_.current_speed)) {
         current_speed = *opt;
-    } else {
-        LOG_WARNING("WARNING: %s is not available, using default value %f\n",
-                    keys_.current_speed.data(), current_speed);
     }
 
     // Read raw target speed (default to zero if missing)
     float target_speed = 0.0f;
     if (auto opt = io.get_as<float>(keys_.target_speed)) {
         target_speed = *opt;
-    } else {
-        LOG_WARNING("WARNING: %s is not available, using default value %f\n",
-                    keys_.target_speed.data(), target_speed);
     }
 
-    // Read flag disabling filtering (default to false if missing)
-    bool no_filter = false;
-    if (auto opt = io.get_as<bool>(keys_.speed_filter_flag)) {
-        no_filter = *opt;
-    } else {
-        LOG_WARNING("WARNING: %s is not available, using default value %d\n",
-                    keys_.speed_filter_flag.data(), no_filter);
-    }
+    DEBUG("SpeedFilter[%s]: speed_order=%.2f, current_speed=%.2f, target_speed=%.2f, prev=%.2f\n",
+          keys_.speed_order.data(), static_cast<double>(speed_order),
+          static_cast<double>(current_speed), static_cast<double>(target_speed),
+          static_cast<double>(previous_speed_order_));
 
-    if (!no_filter) {
-        limit_speed_order(&speed_order, target_speed, parameters_.min_speed(),
-                          parameters_.max_speed(), parameters_.max_acceleration());
-    }
-
-    if (parameters_.anti_blocking()) {
-        const float anti_blocking_speed_threshold = parameters_.anti_blocking_speed_threshold();
-        const float anti_blocking_error_threshold = parameters_.anti_blocking_error_threshold();
-        bool below_threshold = etl::absolute(current_speed) < anti_blocking_speed_threshold;
-        bool no_acceleration =
-            etl::absolute(previous_speed_order_ - current_speed) > anti_blocking_error_threshold;
-
-        if (below_threshold && no_acceleration) {
-            anti_blocking_blocked_cycles_nb_++;
-            DEBUG("Anti blocking cycles number: %" PRIu32 "\n",
-                  static_cast<uint32_t>(anti_blocking_blocked_cycles_nb_));
-        } else {
-            anti_blocking_blocked_cycles_nb_ = 0;
-        }
-
-        if (anti_blocking_blocked_cycles_nb_ >
-            parameters_.anti_blocking_blocked_cycles_nb_threshold()) {
-            LOG_WARNING("BLOCKED\n");
-
-            // Write updated pose‐reached status
-            io.set(keys_.pose_reached, target_pose_status_t::blocked);
-        }
-    }
+    // Apply speed/acceleration limits
+    limit_speed_order(&speed_order, target_speed, parameters_.min_speed(), parameters_.max_speed(),
+                      parameters_.max_acceleration());
 
     previous_speed_order_ = speed_order;
 
