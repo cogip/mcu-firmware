75f2668284e9916cdae1401504e19a8eecff19d9
Mathis Lécrivain <lecrivain.mathis@gmail.com>
2025-12-20 10:22:45 +0100

    motion-control: add TelemetryController
    Add a controller that sends motion control data via the telemetry
    system for real-time monitoring and debugging. It reads speed orders,
    current speeds, and feedforward velocities from ControllersIO and
    transmits them with timestamped key hashes.
    
    This enables visualization of PID behavior during tuning: comparing
    commanded vs actual speeds, observing feedforward contributions, and
    diagnosing tracking issues.
    
    Integrated into LINEAR_SPEED_TUNING and ANGULAR_SPEED_TUNING chains.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

---
+++ motion_control/controllers/telemetry_controller/include/telemetry_controller/TelemetryControllerIOKeys.hpp
@@ -0,0 +1,33 @@
+// Copyright (C) 2025 COGIP Robotics association <cogip35@gmail.com>
+// This file is subject to the terms and conditions of the GNU Lesser
+// General Public License v2.1. See the file LICENSE in the top level
+// directory for more details.
+
+/// @ingroup    telemetry_controller Telemetry controller IO keys
+/// @{
+/// @file
+/// @brief      Telemetry controller IO keys
+/// @author     Mathis LECRIVAIN <lecrivain.mathis@gmail.com>
+
+#pragma once
+
+#include <etl/string_view.h>
+
+namespace cogip {
+
+namespace motion_control {
+
+/// @brief Bundle of ControllersIO key names for a TelemetryController.
+struct TelemetryControllerIOKeys
+{
+    etl::string_view speed_order;          ///< e.g. "speed_order"
+    etl::string_view current_speed;        ///< e.g. "current_speed"
+    etl::string_view speed_command;        ///< e.g. "speed_command"
+    etl::string_view feedforward_velocity; ///< e.g. "feedforward_velocity"
+};
+
+} // namespace motion_control
+
+} // namespace cogip
+
+/// @}
---
+++ motion_control/controllers/telemetry_controller/include/telemetry_controller/TelemetryControllerParameters.hpp
@@ -0,0 +1,27 @@
+// Copyright (C) 2025 COGIP Robotics association <cogip35@gmail.com>
+// This file is subject to the terms and conditions of the GNU Lesser
+// General Public License v2.1. See the file LICENSE in the top level
+// directory for more details.
+
+/// @ingroup    telemetry_controller Telemetry controller parameters
+/// @{
+/// @file
+/// @brief      Telemetry controller parameters
+/// @author     Mathis LECRIVAIN <lecrivain.mathis@gmail.com>
+
+#pragma once
+
+namespace cogip {
+
+namespace motion_control {
+
+/// Telemetry controller parameters
+struct TelemetryControllerParameters
+{
+};
+
+} // namespace motion_control
+
+} // namespace cogip
+
+/// @}
