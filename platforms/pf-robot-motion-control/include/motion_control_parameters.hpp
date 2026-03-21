d958e9d483b9606eb82833f6a8aad734a019b64c
Mathis Lécrivain <lecrivain.mathis@gmail.com>
2026-03-21 14:56:16 +0100

    platforms: pf-robot-motion-control: enable flash storage and parameter loading
    - WithFlashStorage policy added to all non-ReadOnly parameters
      to persist tuned values across reboots
    - Registry now includes both quadpid and tracker chain PID gains,
      fixing previous mapping where quadpid keys pointed to tracker
    - pf_load_parameters() iterates the registry at init to restore
      persisted values before motors and controllers start
    - 200ms delay between flash reads for stability

--- platforms/pf-robot-motion-control/include/motion_control_parameters.hpp
+++ platforms/pf-robot-motion-control/include/motion_control_parameters.hpp
@@ -17,6 +17,7 @@
 #include "app_conf.hpp"
 #include "canpb/ReadBuffer.hpp"
 #include "parameter/Parameter.hpp"
+#include "parameter/StoragePolicies.hpp"
 
 using namespace cogip::parameter;
 using cogip::utils::operator"" _key_hash;
@@ -76,44 +77,44 @@ inline Parameter<float, ReadOnly> qdec_left_polarity{default_qdec_left_polarity}
 inline Parameter<float, ReadOnly> qdec_right_polarity{default_qdec_right_polarity};
 
 /// Encoder parameters
-inline Parameter<float> left_encoder_wheels_diameter_mm{default_left_encoder_wheels_diameter_mm};
-inline Parameter<float> right_encoder_wheels_diameter_mm{default_right_encoder_wheels_diameter_mm};
-inline Parameter<float> encoder_wheels_distance_mm{default_encoder_wheels_distance_mm};
+inline Parameter<float, WithFlashStorage<LEFT_WHEEL_DIAMETER_KEY>> left_encoder_wheels_diameter_mm{default_left_encoder_wheels_diameter_mm};
+inline Parameter<float, WithFlashStorage<RIGHT_WHEEL_DIAMETER_KEY>> right_encoder_wheels_diameter_mm{default_right_encoder_wheels_diameter_mm};
+inline Parameter<float, WithFlashStorage<ENCODER_WHEELS_DISTANCE_KEY>> encoder_wheels_distance_mm{default_encoder_wheels_distance_mm};
 inline Parameter<float, ReadOnly> encoder_wheels_resolution_pulses{default_encoder_wheels_resolution_pulses};
 
 // Linear pose PID (QUADPID chain)
-inline Parameter<float, NonNegative> linear_pose_pid_kp{default_linear_pose_pid_kp};
-inline Parameter<float, NonNegative> linear_pose_pid_ki{default_linear_pose_pid_ki};
-inline Parameter<float, NonNegative> linear_pose_pid_kd{default_linear_pose_pid_kd};
+inline Parameter<float, NonNegative, WithFlashStorage<LINEAR_POSE_PID_KP_KEY>> linear_pose_pid_kp{default_linear_pose_pid_kp};
+inline Parameter<float, NonNegative, WithFlashStorage<LINEAR_POSE_PID_KI_KEY>> linear_pose_pid_ki{default_linear_pose_pid_ki};
+inline Parameter<float, NonNegative, WithFlashStorage<LINEAR_POSE_PID_KD_KEY>> linear_pose_pid_kd{default_linear_pose_pid_kd};
 // Angular pose PID (QUADPID chain)
-inline Parameter<float, NonNegative> angular_pose_pid_kp{default_angular_pose_pid_kp};
-inline Parameter<float, NonNegative> angular_pose_pid_ki{default_angular_pose_pid_ki};
-inline Parameter<float, NonNegative> angular_pose_pid_kd{default_angular_pose_pid_kd};
+inline Parameter<float, NonNegative, WithFlashStorage<ANGULAR_POSE_PID_KP_KEY>> angular_pose_pid_kp{default_angular_pose_pid_kp};
+inline Parameter<float, NonNegative, WithFlashStorage<ANGULAR_POSE_PID_KI_KEY>> angular_pose_pid_ki{default_angular_pose_pid_ki};
+inline Parameter<float, NonNegative, WithFlashStorage<ANGULAR_POSE_PID_KD_KEY>> angular_pose_pid_kd{default_angular_pose_pid_kd};
 // Linear speed PID (QUADPID chain)
-inline Parameter<float, NonNegative> linear_speed_pid_kp{default_linear_speed_pid_kp};
-inline Parameter<float, NonNegative> linear_speed_pid_ki{default_linear_speed_pid_ki};
-inline Parameter<float, NonNegative> linear_speed_pid_kd{default_linear_speed_pid_kd};
+inline Parameter<float, NonNegative, WithFlashStorage<LINEAR_SPEED_PID_KP_KEY>> linear_speed_pid_kp{default_linear_speed_pid_kp};
+inline Parameter<float, NonNegative, WithFlashStorage<LINEAR_SPEED_PID_KI_KEY>> linear_speed_pid_ki{default_linear_speed_pid_ki};
+inline Parameter<float, NonNegative, WithFlashStorage<LINEAR_SPEED_PID_KD_KEY>> linear_speed_pid_kd{default_linear_speed_pid_kd};
 // Angular speed PID (QUADPID chain)
-inline Parameter<float, NonNegative> angular_speed_pid_kp{default_angular_speed_pid_kp};
-inline Parameter<float, NonNegative> angular_speed_pid_ki{default_angular_speed_pid_ki};
-inline Parameter<float, NonNegative> angular_speed_pid_kd{default_angular_speed_pid_kd};
+inline Parameter<float, NonNegative, WithFlashStorage<ANGULAR_SPEED_PID_KP_KEY>> angular_speed_pid_kp{default_angular_speed_pid_kp};
+inline Parameter<float, NonNegative, WithFlashStorage<ANGULAR_SPEED_PID_KI_KEY>> angular_speed_pid_ki{default_angular_speed_pid_ki};
+inline Parameter<float, NonNegative, WithFlashStorage<ANGULAR_SPEED_PID_KD_KEY>> angular_speed_pid_kd{default_angular_speed_pid_kd};
 
 // Tracker linear pose PID
-inline Parameter<float, NonNegative> tracker_linear_pose_pid_kp{default_tracker_linear_pose_pid_kp};
-inline Parameter<float, NonNegative> tracker_linear_pose_pid_ki{default_tracker_linear_pose_pid_ki};
-inline Parameter<float, NonNegative> tracker_linear_pose_pid_kd{default_tracker_linear_pose_pid_kd};
+inline Parameter<float, NonNegative, WithFlashStorage<TRACKER_LINEAR_POSE_PID_KP_KEY>> tracker_linear_pose_pid_kp{default_tracker_linear_pose_pid_kp};
+inline Parameter<float, NonNegative, WithFlashStorage<TRACKER_LINEAR_POSE_PID_KI_KEY>> tracker_linear_pose_pid_ki{default_tracker_linear_pose_pid_ki};
+inline Parameter<float, NonNegative, WithFlashStorage<TRACKER_LINEAR_POSE_PID_KD_KEY>> tracker_linear_pose_pid_kd{default_tracker_linear_pose_pid_kd};
 // Tracker angular pose PID
-inline Parameter<float, NonNegative> tracker_angular_pose_pid_kp{default_tracker_angular_pose_pid_kp};
-inline Parameter<float, NonNegative> tracker_angular_pose_pid_ki{default_tracker_angular_pose_pid_ki};
-inline Parameter<float, NonNegative> tracker_angular_pose_pid_kd{default_tracker_angular_pose_pid_kd};
+inline Parameter<float, NonNegative, WithFlashStorage<TRACKER_ANGULAR_POSE_PID_KP_KEY>> tracker_angular_pose_pid_kp{default_tracker_angular_pose_pid_kp};
+inline Parameter<float, NonNegative, WithFlashStorage<TRACKER_ANGULAR_POSE_PID_KI_KEY>> tracker_angular_pose_pid_ki{default_tracker_angular_pose_pid_ki};
+inline Parameter<float, NonNegative, WithFlashStorage<TRACKER_ANGULAR_POSE_PID_KD_KEY>> tracker_angular_pose_pid_kd{default_tracker_angular_pose_pid_kd};
 // Tracker linear speed PID
-inline Parameter<float, NonNegative> tracker_linear_speed_pid_kp{default_tracker_linear_speed_pid_kp};
-inline Parameter<float, NonNegative> tracker_linear_speed_pid_ki{default_tracker_linear_speed_pid_ki};
-inline Parameter<float, NonNegative> tracker_linear_speed_pid_kd{default_tracker_linear_speed_pid_kd};
+inline Parameter<float, NonNegative, WithFlashStorage<TRACKER_LINEAR_SPEED_PID_KP_KEY>> tracker_linear_speed_pid_kp{default_tracker_linear_speed_pid_kp};
+inline Parameter<float, NonNegative, WithFlashStorage<TRACKER_LINEAR_SPEED_PID_KI_KEY>> tracker_linear_speed_pid_ki{default_tracker_linear_speed_pid_ki};
+inline Parameter<float, NonNegative, WithFlashStorage<TRACKER_LINEAR_SPEED_PID_KD_KEY>> tracker_linear_speed_pid_kd{default_tracker_linear_speed_pid_kd};
 // Tracker angular speed PID
-inline Parameter<float, NonNegative> tracker_angular_speed_pid_kp{default_tracker_angular_speed_pid_kp};
-inline Parameter<float, NonNegative> tracker_angular_speed_pid_ki{default_tracker_angular_speed_pid_ki};
-inline Parameter<float, NonNegative> tracker_angular_speed_pid_kd{default_tracker_angular_speed_pid_kd};
+inline Parameter<float, NonNegative, WithFlashStorage<TRACKER_ANGULAR_SPEED_PID_KP_KEY>> tracker_angular_speed_pid_kp{default_tracker_angular_speed_pid_kp};
+inline Parameter<float, NonNegative, WithFlashStorage<TRACKER_ANGULAR_SPEED_PID_KI_KEY>> tracker_angular_speed_pid_ki{default_tracker_angular_speed_pid_ki};
+inline Parameter<float, NonNegative, WithFlashStorage<TRACKER_ANGULAR_SPEED_PID_KD_KEY>> tracker_angular_speed_pid_kd{default_tracker_angular_speed_pid_kd};
 
 // PID integral limits
 inline Parameter<float, NonNegative> linear_pose_pid_integral_limit{default_linear_pose_pid_integral_limit};
@@ -134,6 +135,9 @@ inline Parameter<float, NonNegative> tracker_angular_speed_pid_integral_limit{de
 namespace cogip {
 namespace pf {
 namespace motion_control {
+
+/// @brief Load all parameters from flash persistent storage
+void pf_load_parameters();
 
 /// @brief Handle parameter get request from canpb
 void pf_handle_parameter_get(cogip::canpb::ReadBuffer& buffer);
